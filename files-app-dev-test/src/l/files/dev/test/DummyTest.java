8a4d7e93c5c3c786d14a52251332aac310d0330c
Lae <llaechen@gmail.com>
2013-04-07 19:01:50 +1200

    Add development projects

---
+++ files-app-dev-test/src/l/files/dev/test/DummyTest.java
@@ -0,0 +1,8 @@
+package l.files.dev.test;
+
+import junit.framework.TestCase;
+
+public class DummyTest extends TestCase {
+
+  // Needed at least a dummy class otherwise test will fail to launch
+}
