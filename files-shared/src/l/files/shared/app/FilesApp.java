aad8c032d5e8751f27e40435d1189a376a1580ce
Lae <llaechen@gmail.com>
2013-04-17 21:44:32 +1200

    Removed dev and release project

--- files-shared/src/l/files/shared/app/FilesApp.java
+++ files-shared/src/l/files/shared/app/FilesApp.java
@@ -1,9 +1,11 @@
 package l.files.shared.app;
 
 import static android.preference.PreferenceManager.getDefaultSharedPreferences;
+import static l.files.shared.BuildConfig.DEBUG;
 import l.files.shared.event.EventBus;
 import android.app.Application;
 import android.content.Context;
+import android.os.StrictMode;
 import android.support.v4.app.Fragment;
 
 public class FilesApp extends Application {
@@ -23,6 +25,7 @@ public class FilesApp extends Application {
   @Override public void onCreate() {
     super.onCreate();
     settings = new Settings(this, getDefaultSharedPreferences(this));
+    if (DEBUG) StrictMode.enableDefaults();
   }
 
   public Settings getSettings() {
