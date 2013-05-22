e4b71a2115c166a2b0e6e470b226ea59934483f0
Lae <llaechen@gmail.com>
2013-05-22 22:11:42 +1200

    Options menu action interface.

---
+++ Files/src/l/files/ui/action/OptionsMenuAction.java
@@ -0,0 +1,25 @@
+package l.files.ui.action;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * An action for a single options menu item.
+ * {@link #onOptionsItemSelected(MenuItem)} will only be called if the ID of the
+ * menu item is the same as {@link #getItemId()}.
+ */
+public interface OptionsMenuAction {
+
+  /**
+   * Returns the item ID of this option menu action, or 0 if this action has no
+   * visual item.
+   */
+  int getItemId();
+
+  void onCreateOptionsMenu(Menu menu);
+
+  void onPrepareOptionsMenu(Menu menu);
+
+  void onOptionsItemSelected(MenuItem item);
+
+}
---
+++ Files/src/l/files/ui/action/OptionsMenuActionAdapter.java
@@ -0,0 +1,20 @@
+package l.files.ui.action;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class OptionsMenuActionAdapter implements OptionsMenuAction {
+
+  @Override public int getItemId() {
+    return 0;
+  }
+
+  @Override public void onCreateOptionsMenu(Menu menu) {
+  }
+
+  @Override public void onPrepareOptionsMenu(Menu menu) {
+  }
+
+  @Override public void onOptionsItemSelected(MenuItem item) {
+  }
+}
