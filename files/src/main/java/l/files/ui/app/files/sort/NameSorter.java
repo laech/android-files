1aae91c1acb6f86c4bbe434614b18921736fa16f
Lae <llaechen@gmail.com>
2013-07-01 20:08:02 +1200

    NameSorter.

---
+++ files/src/main/java/l/files/ui/app/files/sort/NameSorter.java
@@ -0,0 +1,39 @@
+package l.files.ui.app.files.sort;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static java.lang.String.CASE_INSENSITIVE_ORDER;
+import static java.util.Collections.sort;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import l.files.R;
+import l.files.setting.SortBy;
+import android.content.res.Resources;
+
+final class NameSorter implements Sorter {
+
+  private static final Comparator<File> BY_NAME = new Comparator<File>() {
+    @Override public int compare(File x, File y) {
+      return CASE_INSENSITIVE_ORDER.compare(x.getName(), y.getName());
+    }
+  };
+
+  @Override public List<Object> apply(Collection<File> files) {
+    List<File> result = newArrayList(files);
+    sort(result, BY_NAME);
+    return Collections.<Object> unmodifiableList(result);
+  }
+
+  @Override public SortBy id() {
+    return SortBy.NAME;
+  }
+
+  @Override public String name(Resources res) {
+    return res.getString(R.string.name);
+  }
+
+}
---
+++ files/src/main/java/l/files/ui/app/files/sort/Sorter.java
@@ -0,0 +1,23 @@
+package l.files.ui.app.files.sort;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.List;
+
+import l.files.setting.SortBy;
+import android.content.res.Resources;
+
+import com.google.common.base.Function;
+
+/**
+ * A sorter for an associated {@link SortBy} for sorting files to be displayed
+ * in a list, optionally can inject extra elements such as headers into the
+ * resulting list.
+ */
+public interface Sorter extends Function<Collection<File>, List<Object>> {
+
+  SortBy id();
+
+  String name(Resources res);
+
+}
