60f6560169c42c077fdec3fa4b862f4f86f32b10
Lae <llaechen@gmail.com>
2013-07-03 13:00:18 +1200

    Delete unused methods.

--- files/src/main/java/l/files/ui/util/ListViews.java
+++ files/src/main/java/l/files/ui/util/ListViews.java
@@ -1,19 +1,13 @@
 package l.files.ui.util;
 
-import android.view.View;
 import android.widget.AbsListView;
-import android.widget.ListView;
 
 import java.util.List;
 
 import static com.google.common.collect.Iterables.filter;
 import static com.google.common.collect.Lists.newArrayListWithCapacity;
-import static java.util.Collections.emptyList;
 
 public final class ListViews {
-
-  private ListViews() {
-  }
 
   public static List<Object> getCheckedItems(AbsListView list) {
     int n = list.getCheckedItemCount();
@@ -29,21 +23,6 @@ public final class ListViews {
     return filter(getCheckedItems(list), type);
   }
 
-  public static List<Object> getVisibleItems(AbsListView listView) {
-    int count = listView.getChildCount();
-    if (0 == count) return emptyList();
+  private ListViews() {}
 
-    List<Object> visibleItems = newArrayListWithCapacity(count);
-    int first = listView.getFirstVisiblePosition();
-    int last = listView.getLastVisiblePosition();
-    for (int i = first; i <= last; i++)
-      visibleItems.add(listView.getItemAtPosition(i));
-
-    return visibleItems;
-  }
-
-  public static int getFirstChildScrollOffset(ListView listView) {
-    View view = listView.getChildAt(0);
-    return view == null ? 0 : view.getTop();
-  }
 }
