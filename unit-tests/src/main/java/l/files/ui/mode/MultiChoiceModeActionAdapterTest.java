94d1408ed4c0161fda476862d54af163ce64d5f6
Lae <llaechen@gmail.com>
2013-07-06 16:49:52 +1200

    Move MultiChoiceModeActionAdapterTest.

--- unit-tests/src/main/java/l/files/ui/mode/MultiChoiceModeActionAdapterTest.java
+++ unit-tests/src/main/java/l/files/ui/mode/MultiChoiceModeActionAdapterTest.java
@@ -4,19 +4,19 @@ import junit.framework.TestCase;
 
 public final class MultiChoiceModeActionAdapterTest extends TestCase {
 
-  public void testOnCreateActionModeReturnsTrueToIndicateActionModeShouldBeCreatedByDefault() {
+  public void testOnCreateActionMode_returnsTrueToIndicateActionModeShouldBeCreatedByDefault() {
     assertTrue(create().onCreateActionMode(null, null));
   }
 
-  public void testOnActionItemClickedReturnsFalseToIndicateActionIsNotHandledByDefault() {
+  public void testOnActionItemClicked_returnsFalseToIndicateActionIsNotHandledByDefault() {
     assertFalse(create().onActionItemClicked(null, null));
   }
 
-  public void testOnPrepareActionModeReturnsFalseToIndicateNoLayoutChangeByDefault() {
+  public void testOnPrepareActionMode_returnsFalseToIndicateNoLayoutChangeByDefault() {
     assertFalse(create().onPrepareActionMode(null, null));
   }
 
-  public void testGetItemIdReturns0ToIndicateNoItemIsCreatedByDefault() {
+  public void testGetItemId_returns0ToIndicateNoItemIsCreatedByDefault() {
     assertEquals(0, create().getItemId());
   }
 
