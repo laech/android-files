4721a1d6ded30f93932c039c0cdcdc94f6875b69
Lae <llaechen@gmail.com>
2013-07-29 21:00:32 +1200

    Restructure decorators.

--- files-common/src/l/files/common/widget/Decorator.java
+++ files-common/src/l/files/common/widget/Decorator.java
@@ -2,7 +2,17 @@ package l.files.common.widget;
 
 import android.view.View;
 
+/**
+ * Decorates a view with extra properties from an item.
+ *
+ * @param <T> the item type
+ */
 public interface Decorator<T> {
-
+  /**
+   * Decorates the view with the item.
+   *
+   * @param view the view to be decorated
+   * @param item the model of this view
+   */
   void decorate(View view, T item);
 }
