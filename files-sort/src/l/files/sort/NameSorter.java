10fd612a286de3fa04818610dbb0048333f7d439
Lae <llaechen@gmail.com>
2013-10-05 11:36:01 +1300

    Sort by name more efficiently on large directories.

--- files-sort/src/l/files/sort/NameSorter.java
+++ files-sort/src/l/files/sort/NameSorter.java
@@ -3,29 +3,30 @@ package l.files.sort;
 import android.content.res.Resources;
 
 import java.io.File;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static java.lang.String.CASE_INSENSITIVE_ORDER;
-import static java.util.Collections.sort;
+import static java.util.Arrays.sort;
 
 final class NameSorter implements SortHelper {
 
-  private static final Comparator<File> BY_NAME = new Comparator<File>() {
-    @Override public int compare(File x, File y) {
-      return CASE_INSENSITIVE_ORDER.compare(x.getName(), y.getName());
+    private static final Comparator<File> BY_NAME = new Comparator<File>() {
+        @Override
+        public int compare(File x, File y) {
+            return CASE_INSENSITIVE_ORDER.compare(x.getName(), y.getName());
+        }
+    };
+
+    @Override
+    public List<Object> apply(Resources res, File... files) {
+        sort(files, BY_NAME);
+        return Arrays.<Object>asList(files);
     }
-  };
 
-  @Override public List<Object> apply(Resources res, File... files) {
-    List<File> result = newArrayList(files);
-    sort(result, BY_NAME);
-    return Collections.<Object>unmodifiableList(result);
-  }
-
-  @Override public String name(Resources res) {
-    return res.getString(R.string.name);
-  }
+    @Override
+    public String name(Resources res) {
+        return res.getString(R.string.name);
+    }
 }
