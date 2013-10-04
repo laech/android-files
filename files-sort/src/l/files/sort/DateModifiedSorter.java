5d737f0ac789157183b96e3140cf59570c252434
Lae <llaechen@gmail.com>
2013-10-05 11:28:18 +1300

    Sort by date more efficient on large directories.

--- files-sort/src/l/files/sort/DateModifiedSorter.java
+++ files-sort/src/l/files/sort/DateModifiedSorter.java
@@ -1,86 +1,96 @@
 package l.files.sort;
 
 import android.content.res.Resources;
-import com.google.common.base.Function;
-import com.google.common.collect.Multimap;
-import com.google.common.collect.Multimaps;
 import com.google.common.primitives.Longs;
+import gnu.trove.map.TObjectLongMap;
+import gnu.trove.map.hash.TObjectLongHashMap;
 import org.joda.time.DateMidnight;
 
 import java.io.File;
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 
-import static com.google.common.collect.Lists.newArrayList;
-import static java.util.Collections.sort;
+import static com.google.common.collect.Lists.newArrayListWithCapacity;
+import static java.util.Arrays.sort;
+import static l.files.sort.DateModifiedSorter.When.*;
 import static org.joda.time.DateTimeConstants.MILLIS_PER_DAY;
 
 final class DateModifiedSorter implements SortHelper {
 
-  private static enum When {
+    static enum When {
 
-    // Ordered as they will be displayed on UI
+        // Ordered as they will be displayed on UI
 
-    UNKNOWN(R.string.unknown),
-    TODAY(R.string.today),
-    YESTERDAY(R.string.yesterday),
-    PREVIOUS_7_DAYS(R.string.previous_7_days),
-    PREVIOUS_30_DAYS(R.string.previous_30_days),
-    EARLIER(R.string.earlier);
+        UNKNOWN(R.string.unknown),
+        TODAY(R.string.today),
+        YESTERDAY(R.string.yesterday),
+        PREVIOUS_7_DAYS(R.string.previous_7_days),
+        PREVIOUS_30_DAYS(R.string.previous_30_days),
+        EARLIER(R.string.earlier);
 
-    final int stringResId;
+        final int stringResId;
 
-    When(int stringResId) {
-      this.stringResId = stringResId;
+        When(int stringResId) {
+            this.stringResId = stringResId;
+        }
     }
-  }
 
-  private static final Comparator<File> BY_DATE_MODIFIED_DESC = new Comparator<File>() {
-    @Override public int compare(File x, File y) {
-      return Longs.compare(x.lastModified(), y.lastModified()) * -1;
+    @Override
+    public List<Object> apply(Resources res, File... files) {
+        final TObjectLongMap<File> timestamps = getLastModifiedTimestamps(files);
+        sortByLastModifiedDesc(files, timestamps);
+
+        final long startOfToday = DateMidnight.now().getMillis();
+        final long startOfTomorrow = startOfToday + MILLIS_PER_DAY;
+        final long startOfYesterday = startOfToday - MILLIS_PER_DAY;
+        final long startOf7Days = startOfToday - MILLIS_PER_DAY * 7L;
+        final long startOf30Days = startOfToday - MILLIS_PER_DAY * 30L;
+
+        final List<Object> result = newArrayListWithCapacity(files.length + 5);
+        When current = null;
+        for (File file : files) {
+            final long modified = timestamps.get(file);
+
+            final When when;
+            if (modified >= startOfTomorrow) when = UNKNOWN;
+            else if (modified >= startOfToday) when = TODAY;
+            else if (modified >= startOfYesterday) when = YESTERDAY;
+            else if (modified >= startOf7Days) when = PREVIOUS_7_DAYS;
+            else if (modified >= startOf30Days) when = PREVIOUS_30_DAYS;
+            else when = EARLIER;
+
+            if (current != when) {
+                current = when;
+                result.add(res.getString(when.stringResId));
+            }
+            result.add(file);
+        }
+        return result;
     }
-  };
 
-  @Override public List<Object> apply(Resources res, File... files) {
-    List<File> result = newArrayList(files);
-    sort(result, BY_DATE_MODIFIED_DESC);
-    return toList(groupByDateModified(result), res);
-  }
-
-  private Multimap<When, File> groupByDateModified(List<File> files) {
-    final long startOfToday = DateMidnight.now().getMillis();
-    final long startOfTomorrow = startOfToday + MILLIS_PER_DAY;
-    final long startOfYesterday = startOfToday - MILLIS_PER_DAY;
-    final long startOf7Days = startOfToday - MILLIS_PER_DAY * 7L;
-    final long startOf30Days = startOfToday - MILLIS_PER_DAY * 30L;
-
-    return Multimaps.index(files, new Function<File, When>() {
-      @Override public When apply(File file) {
-        long time = file.lastModified();
-        if (time >= startOfTomorrow) return When.UNKNOWN;
-        if (time >= startOfToday) return When.TODAY;
-        if (time >= startOfYesterday) return When.YESTERDAY;
-        if (time >= startOf7Days) return When.PREVIOUS_7_DAYS;
-        if (time >= startOf30Days) return When.PREVIOUS_30_DAYS;
-        return When.EARLIER;
-      }
-    });
-  }
-
-  private List<Object> toList(Multimap<When, File> groups, Resources res) {
-    List<Object> result = newArrayList();
-    for (When when : When.values()) {
-      Collection<File> col = groups.get(when);
-      if (!col.isEmpty()) {
-        result.add(res.getString(when.stringResId));
-        result.addAll(col);
-      }
+    private void sortByLastModifiedDesc(File[] files, final TObjectLongMap<File> timestamps) {
+        sort(files, new Comparator<File>() {
+            @Override
+            public int compare(File a, File b) {
+                long x = timestamps.get(a);
+                long y = timestamps.get(b);
+                return Longs.compare(y, x);
+            }
+        });
     }
-    return result;
-  }
 
-  @Override public String name(Resources res) {
-    return res.getString(R.string.date_modified);
-  }
+    private TObjectLongMap<File> getLastModifiedTimestamps(File[] files) {
+        final float loadFactor = 0.75f;
+        final int initialCapacity = (int) ((files.length + 1) / loadFactor);
+        final TObjectLongMap<File> timestamps = new TObjectLongHashMap<File>(initialCapacity, loadFactor);
+        for (File file : files) {
+            timestamps.put(file, file.lastModified());
+        }
+        return timestamps;
+    }
+
+    @Override
+    public String name(Resources res) {
+        return res.getString(R.string.date_modified);
+    }
 }
