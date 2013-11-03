c1701b64a8c1b1e646d25166ef2ed5e0edef9ed0
Lae <llaechen@gmail.com>
2013-11-03 18:13:23 +1300

    Disable changing transition to give overall smoother experience.

--- files-app/src/l/files/app/ViewPagerTabBar.java
+++ files-app/src/l/files/app/ViewPagerTabBar.java
@@ -1,7 +1,6 @@
 package l.files.app;
 
 import android.animation.LayoutTransition;
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -15,10 +14,7 @@ import android.widget.TextView;
 import com.squareup.otto.Bus;
 import l.files.R;
 
-import static android.animation.LayoutTransition.CHANGING;
 import static android.animation.LayoutTransition.TransitionListener;
-import static android.os.Build.VERSION.SDK_INT;
-import static android.os.Build.VERSION_CODES.JELLY_BEAN;
 import static android.support.v4.view.ViewPager.OnPageChangeListener;
 import static android.view.View.*;
 import static android.view.ViewGroup.LayoutParams;
@@ -27,182 +23,170 @@ import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public final class ViewPagerTabBar
-        implements OnPageChangeListener, OnClickListener, TransitionListener {
+    implements OnPageChangeListener, OnClickListener, TransitionListener {
 
-    private final HorizontalScrollView mRoot;
-    private final LinearLayout mTabs;
-    private final LayoutTransition mTransition;
-    private final Bus mBus;
-    private ViewPager mPager;
+  private final HorizontalScrollView root;
+  private final LinearLayout tabs;
+  private final Bus bus;
+  private ViewPager pager;
 
-    public ViewPagerTabBar(Context context, Bus bus) {
-        mBus = checkNotNull(bus, "bus");
-        mRoot = (HorizontalScrollView) inflate(context, R.layout.tab_container);
-        mRoot.setHorizontalFadingEdgeEnabled(true);
-        mTabs = (LinearLayout) mRoot.findViewById(R.id.tab_container);
-        mTransition = mTabs.getLayoutTransition();
-        mTransition.addTransitionListener(this);
-        if (SDK_INT >= JELLY_BEAN) {
-            enableChangingTransition();
-        }
+  public ViewPagerTabBar(Context context, Bus bus) {
+    this.bus = checkNotNull(bus, "bus");
+    this.root = (HorizontalScrollView) inflate(context, R.layout.tab_container);
+    this.root.setHorizontalFadingEdgeEnabled(true);
+    this.tabs = (LinearLayout) root.findViewById(R.id.tab_container);
+    this.tabs.getLayoutTransition().addTransitionListener(this);
+  }
+
+  public void setViewPager(ViewPager p) {
+    if (pager != null) {
+      throw new IllegalStateException("ViewPager is already set");
+    }
+    pager = p;
+    pager.setOnPageChangeListener(this);
+    PagerAdapter adapter = p.getAdapter();
+    for (int i = 0; i < adapter.getCount(); i++) {
+      addTab(adapter.getPageTitle(i));
+    }
+    onPageSelected(p.getCurrentItem());
+  }
+
+  public View getRootContainer() {
+    return root;
+  }
+
+  public Tab getTabAt(int position) {
+    return (Tab) tabs.getChildAt(position).getTag();
+  }
+
+  public int getTabCount() {
+    return tabs.getChildCount();
+  }
+
+  public void addTab(CharSequence title) {
+    Tab holder = Tab.create(inflate(pager.getContext(), R.layout.tab));
+    holder.root.setOnClickListener(this);
+    holder.title.setText(title);
+    tabs.addView(holder.root, new LayoutParams(WRAP_CONTENT, MATCH_PARENT));
+  }
+
+  public void removeTab(int position) {
+    tabs.removeViewAt(position);
+  }
+
+  public void updateTab(int position, CharSequence title, boolean showBack) {
+    View view = tabs.getChildAt(position);
+    if (view != null) {
+      Tab holder = Tab.get(view);
+      holder.title.setText(title);
+      holder.backIndicator.setVisibility(showBack ? VISIBLE : GONE);
+      scrollToTab(pager.getCurrentItem());
+    }
+  }
+
+  private void updateTabsStatus(int currentPosition) {
+    for (int i = 0; i < tabs.getChildCount(); i++) {
+      boolean selected = i == currentPosition;
+      Tab holder = Tab.get(tabs.getChildAt(i));
+      holder.root.setSelected(selected);
+      if (!selected) {
+        holder.backIndicator.setVisibility(GONE);
+      }
+    }
+  }
+
+  private void scrollToTab(int position) {
+    scrollToTab(tabs.getChildAt(position));
+  }
+
+  private void scrollToTab(View tab) {
+    int tabLeft = tab.getLeft();
+    int tabRight = tabLeft + tab.getMeasuredWidth();
+    int scrollLeft = root.getScrollX();
+    int scrollRight = scrollLeft + root.getMeasuredWidth();
+    int padding = 100;
+    if (tabLeft < scrollLeft) {
+      root.smoothScrollBy(tabLeft - scrollLeft - padding, 0);
+    } else if (tabRight > scrollRight) {
+      root.smoothScrollBy(tabRight - scrollRight + padding, 0);
+    }
+  }
+
+  @Override public void onClick(View v) {
+    if (v.isSelected()) {
+      bus.post(OnUpSelected.INSTANCE);
+    } else {
+      pager.setCurrentItem(tabs.indexOfChild(v), true);
+    }
+  }
+
+  @Override public void onPageSelected(int position) {
+    updateTabsStatus(position);
+    scrollToTab(position);
+  }
+
+  @Override public void onPageScrollStateChanged(int state) {
+  }
+
+  @Override
+  public void onPageScrolled(int position, float positionOffset,
+                             int positionOffsetPixels) {
+  }
+
+  @Override
+  public void startTransition(LayoutTransition transition, ViewGroup container,
+                              View view, int transitionType) {
+  }
+
+  @Override
+  public void endTransition(LayoutTransition transition, ViewGroup container,
+                            View view, int transitionType) {
+    int position = container.indexOfChild(view);
+    if (position == pager.getCurrentItem()) {
+      scrollToTab(position);
+    }
+    updateTabsStatus(pager.getCurrentItem());
+  }
+
+  private View inflate(Context context, int layoutResId) {
+    return LayoutInflater.from(context).inflate(layoutResId, null, false);
+  }
+
+  public static final class Tab {
+    final View root;
+    final TextView title;
+    final ImageView backIndicator;
+
+    private Tab(View root) {
+      this.root = root;
+      this.title = (TextView) root.findViewById(R.id.title);
+      this.backIndicator = (ImageView) root.findViewById(R.id.back_indicator);
     }
 
-    @TargetApi(JELLY_BEAN)
-    private void enableChangingTransition() {
-        mTransition.enableTransitionType(CHANGING);
+    static Tab create(View root) {
+      Tab holder = new Tab(root);
+      root.setTag(holder);
+      return holder;
     }
 
-    public void setViewPager(ViewPager p) {
-        if (mPager != null) {
-            throw new IllegalStateException("ViewPager is already set");
-        }
-        mPager = p;
-        mPager.setOnPageChangeListener(this);
-        final PagerAdapter adapter = p.getAdapter();
-        for (int i = 0; i < adapter.getCount(); i++) {
-            addTab(adapter.getPageTitle(i));
-        }
-        onPageSelected(p.getCurrentItem());
+    static Tab get(View root) {
+      return (Tab) root.getTag();
     }
 
-    public View getRootContainer() {
-        return mRoot;
+    public View getRootView() {
+      return root;
     }
 
-    @SuppressWarnings("ConstantConditions")
-    public Tab getTabAt(int position) {
-        return (Tab) mTabs.getChildAt(position).getTag();
+    public TextView getTitleView() {
+      return title;
     }
 
-    public int getTabCount() {
-        return mTabs.getChildCount();
+    public ImageView getBackIndicatorView() {
+      return backIndicator;
     }
+  }
 
-    public void addTab(CharSequence title) {
-        final Tab holder = Tab.create(inflate(mPager.getContext(), R.layout.tab));
-        holder.mRoot.setOnClickListener(this);
-        holder.mTitle.setText(title);
-        mTabs.addView(holder.mRoot, new LayoutParams(WRAP_CONTENT, MATCH_PARENT));
-    }
-
-    public void removeTab(int position) {
-        mTabs.removeViewAt(position);
-    }
-
-    public void updateTab(final int position, CharSequence title, boolean showBack) {
-        final View view = mTabs.getChildAt(position);
-        if (view != null) {
-            final Tab holder = Tab.get(view);
-            holder.mTitle.setText(title);
-            holder.mBackIndicator.setVisibility(showBack ? VISIBLE : GONE);
-        }
-    }
-
-    private void updateTabsStatus(int currentPosition) {
-        for (int i = 0; i < mTabs.getChildCount(); i++) {
-            final boolean selected = i == currentPosition;
-            final Tab holder = Tab.get(mTabs.getChildAt(i));
-            holder.mRoot.setSelected(selected);
-            if (!selected) {
-                holder.mBackIndicator.setVisibility(GONE);
-            }
-        }
-    }
-
-    private void scrollToTab(int position) {
-        scrollToTab(mTabs.getChildAt(position));
-    }
-
-    private void scrollToTab(View tab) {
-        final int tabLeft = tab.getLeft();
-        final int tabRight = tabLeft + tab.getMeasuredWidth();
-        final int scrollLeft = mRoot.getScrollX();
-        final int scrollRight = scrollLeft + mRoot.getMeasuredWidth();
-        final int padding = 100;
-        if (tabLeft < scrollLeft) {
-            mRoot.smoothScrollBy(tabLeft - scrollLeft - padding, 0);
-        } else if (tabRight > scrollRight) {
-            mRoot.smoothScrollBy(tabRight - scrollRight + padding, 0);
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.isSelected()) {
-            mBus.post(OnUpSelected.INSTANCE);
-        } else {
-            mPager.setCurrentItem(mTabs.indexOfChild(v), true);
-        }
-    }
-
-    @Override
-    public void onPageSelected(int position) {
-        updateTabsStatus(position);
-        if (!mTransition.isRunning()) {
-            scrollToTab(position);
-        }
-    }
-
-    @Override
-    public void onPageScrollStateChanged(int state) {
-    }
-
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-    }
-
-    @Override
-    public void startTransition(LayoutTransition transition, ViewGroup container, View view, int transitionType) {
-    }
-
-    @Override
-    public void endTransition(LayoutTransition transition, ViewGroup container, View view, int transitionType) {
-        final int position = container.indexOfChild(view);
-        if (position == mPager.getCurrentItem()) {
-            scrollToTab(position);
-        }
-        updateTabsStatus(mPager.getCurrentItem());
-    }
-
-    private View inflate(Context context, int layoutResId) {
-        return LayoutInflater.from(context).inflate(layoutResId, null, false);
-    }
-
-    public static final class Tab {
-        final View mRoot;
-        final TextView mTitle;
-        final ImageView mBackIndicator;
-
-        private Tab(View root) {
-            this.mRoot = root;
-            this.mTitle = (TextView) root.findViewById(R.id.title);
-            this.mBackIndicator = (ImageView) root.findViewById(R.id.back_indicator);
-        }
-
-        static Tab create(View root) {
-            final Tab holder = new Tab(root);
-            root.setTag(holder);
-            return holder;
-        }
-
-        static Tab get(View root) {
-            return (Tab) root.getTag();
-        }
-
-        public View getRootView() {
-            return mRoot;
-        }
-
-        public TextView getTitleView() {
-            return mTitle;
-        }
-
-        public ImageView getBackIndicatorView() {
-            return mBackIndicator;
-        }
-    }
-
-    enum OnUpSelected {
-        INSTANCE
-    }
+  enum OnUpSelected {
+    INSTANCE
+  }
 }
