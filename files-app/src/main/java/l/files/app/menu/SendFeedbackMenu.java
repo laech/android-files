bbf988ea0f781779d45b02af1f537f5164fd2f3b
Lae <llaechen@gmail.com>
2013-12-09 21:26:16 +1300

    Refactor send feedback.

--- files-app/src/main/java/l/files/app/menu/SendFeedbackMenu.java
+++ files-app/src/main/java/l/files/app/menu/SendFeedbackMenu.java
@@ -1,15 +1,13 @@
 package l.files.app.menu;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import l.files.R;
+import l.files.app.util.GoogleFeedback;
 import l.files.common.app.OptionsMenuAction;
 
-import static android.content.Context.BIND_AUTO_CREATE;
-import static android.content.Intent.ACTION_BUG_REPORT;
 import static android.view.Menu.CATEGORY_SECONDARY;
 import static android.view.Menu.NONE;
 import static android.view.MenuItem.SHOW_AS_ACTION_NEVER;
@@ -34,9 +32,6 @@ public final class SendFeedbackMenu extends OptionsMenuAction {
   }
 
   @Override protected void onItemSelected(MenuItem item) {
-    activity.bindService(
-        new Intent(ACTION_BUG_REPORT),
-        new FeedbackConnection(activity),
-        BIND_AUTO_CREATE);
+    GoogleFeedback.send(activity);
   }
 }
---
+++ files-app/src/main/java/l/files/app/util/GoogleFeedback.java
@@ -0,0 +1,64 @@
+package l.files.app.util;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.graphics.Bitmap;
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+import android.util.Log;
+
+import com.google.common.base.Optional;
+
+import static android.content.Context.BIND_AUTO_CREATE;
+import static android.content.Intent.ACTION_BUG_REPORT;
+import static android.os.IBinder.FIRST_CALL_TRANSACTION;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Allows user to send feedback using the system feedback framework.
+ */
+public final class GoogleFeedback implements ServiceConnection {
+
+  private static final String TAG = GoogleFeedback.class.getSimpleName();
+
+  private final Activity activity;
+
+  private GoogleFeedback(Activity activity) {
+    this.activity = checkNotNull(activity, "activity");
+  }
+
+  /**
+   * Invoke to allow the user to send a feedback for this app.
+   *
+   * @param activity the activity invoking this, a screenshot will be taken
+   */
+  public static void send(Activity activity) {
+    activity.bindService(
+        new Intent(ACTION_BUG_REPORT),
+        new GoogleFeedback(activity),
+        BIND_AUTO_CREATE);
+  }
+
+  @Override
+  public void onServiceConnected(ComponentName name, IBinder service) {
+    try {
+      Optional<Bitmap> screenshot = Screenshots.take(activity);
+      Parcel parcel = Parcel.obtain();
+      if (screenshot.isPresent()) {
+        screenshot.get().writeToParcel(parcel, 0);
+        screenshot.get().recycle();
+      }
+      service.transact(FIRST_CALL_TRANSACTION, parcel, null, 0);
+
+    } catch (RemoteException e) {
+      Log.e(TAG, e.getMessage(), e); // TODO track
+    } finally {
+      activity.unbindService(this);
+    }
+  }
+
+  @Override public void onServiceDisconnected(ComponentName name) {}
+}
