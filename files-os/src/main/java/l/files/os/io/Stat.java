b6bddbb5708b320849bfe282580cea0de7741f39
Lae <llaechen@gmail.com>
2014-03-18 22:15:39 +1300

    Add constants.

--- files-os/src/main/java/l/files/os/io/Stat.java
+++ files-os/src/main/java/l/files/os/io/Stat.java
@@ -20,7 +20,45 @@ package l.files.os.io;
  * File information returned by fstat(2), lstat(2), and stat(2). Corresponds to
  * C's {@code struct stat} from <a href="http://www.opengroup.org/onlinepubs/000095399/basedefs/sys/stat.h.html">&lt;stat.h&gt;</a>
  */
+@SuppressWarnings("OctalInteger")
 public final class Stat {
+
+  /* See /usr/include/linux/stat.h for meaning of these constants. */
+
+  public static final int S_IFMT = 00170000;
+  public static final int S_IFSOCK = 0140000;
+  public static final int S_IFLNK = 0120000;
+  public static final int S_IFREG = 0100000;
+  public static final int S_IFBLK = 0060000;
+  public static final int S_IFDIR = 0040000;
+  public static final int S_IFCHR = 0020000;
+  public static final int S_IFIFO = 0010000;
+  public static final int S_ISUID = 0004000;
+  public static final int S_ISGID = 0002000;
+  public static final int S_ISVTX = 0001000;
+
+  public static boolean S_ISLNK(int m) {return (m & S_IFMT) == S_IFLNK;}
+  public static boolean S_ISREG(int m) {return (m & S_IFMT) == S_IFREG;}
+  public static boolean S_ISDIR(int m) {return (m & S_IFMT) == S_IFDIR;}
+  public static boolean S_ISCHR(int m) {return (m & S_IFMT) == S_IFCHR;}
+  public static boolean S_ISBLK(int m) {return (m & S_IFMT) == S_IFBLK;}
+  public static boolean S_ISFIFO(int m) {return (m & S_IFMT) == S_IFIFO;}
+  public static boolean S_ISSOCK(int m) {return (m & S_IFMT) == S_IFSOCK;}
+
+  public static final int S_IRWXU = 00700;
+  public static final int S_IRUSR = 00400;
+  public static final int S_IWUSR = 00200;
+  public static final int S_IXUSR = 00100;
+
+  public static final int S_IRWXG = 00070;
+  public static final int S_IRGRP = 00040;
+  public static final int S_IWGRP = 00020;
+  public static final int S_IXGRP = 00010;
+
+  public static final int S_IRWXO = 00007;
+  public static final int S_IROTH = 00004;
+  public static final int S_IWOTH = 00002;
+  public static final int S_IXOTH = 00001;
 
   /**
    * Device ID of device containing file.
