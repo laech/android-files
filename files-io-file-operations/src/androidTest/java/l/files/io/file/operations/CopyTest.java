c1ee3e529447c4a4cfc57b49a9bf17ce9c4258c1
Lae <llaechen@gmail.com>
2014-06-09 22:42:18 +1200

    Update Copy to notify on path instead of FileInfo.

--- files-io-file-operations/src/androidTest/java/l/files/io/file/operations/CopyTest.java
+++ files-io-file-operations/src/androidTest/java/l/files/io/file/operations/CopyTest.java
@@ -1,19 +1,14 @@
 package l.files.io.file.operations;
 
-import com.google.common.base.Function;
 import com.google.common.io.Files;
 
 import org.mockito.ArgumentCaptor;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Set;
-
-import l.files.io.file.FileInfo;
+import java.util.List;
 
 import static com.google.common.base.Charsets.UTF_8;
-import static com.google.common.collect.Lists.transform;
-import static com.google.common.collect.Sets.newHashSet;
 import static com.google.common.io.Files.write;
 import static java.util.Arrays.asList;
 import static l.files.io.file.Files.readlink;
@@ -25,98 +20,91 @@ import static org.mockito.Mockito.verify;
 
 public final class CopyTest extends PasteTest {
 
-  private static final Listener NULL_LISTENER = new Listener() {
-    @Override public void onCopy(FileInfo src, FileInfo dst) {}
-  };
-
-  public void testNotifiesListener() throws Exception {
-    File srcDir = tmp().createDir("a");
-    tmp().createFile("a/file");
-    File dstDir = tmp().createDir("dir");
-
-    Listener listener = mock(Listener.class);
-    ArgumentCaptor<FileInfo> srcCaptor = ArgumentCaptor.forClass(FileInfo.class);
-    ArgumentCaptor<FileInfo> dstCaptor = ArgumentCaptor.forClass(FileInfo.class);
-
-    create(listener, asList(srcDir.getPath()), dstDir.getPath()).call();
-    verify(listener, atLeastOnce()).onCopy(srcCaptor.capture(), dstCaptor.capture());
-
-    Set<String> srcExpected = newHashSet(
-        tmp().get("a").getPath(),
-        tmp().get("a/file").getPath()
-    );
-    Set<String> dstExpected = newHashSet(
-        tmp().get("dir/a").getPath(),
-        tmp().get("dir/a/file").getPath()
-    );
-    Set<String> srcActual = getPaths(srcCaptor);
-    Set<String> dstActual = getPaths(dstCaptor);
-
-    assertEquals(srcExpected, srcActual);
-    assertEquals(dstExpected, dstActual);
-  }
-
-  private Set<String> getPaths(ArgumentCaptor<FileInfo> captor) {
-    return newHashSet(transform(captor.getAllValues(),
-        new Function<FileInfo, String>() {
-          @Override public String apply(FileInfo input) {
-            return input.getPath();
-          }
+    private static final Listener NULL_LISTENER = new Listener() {
+        @Override
+        public void onCopy(String src, String dst) {
         }
-    ));
-  }
+    };
 
-  public void testCopiesSymlink() throws Exception {
-    File target = tmp().createFile("target");
-    File link = tmp().get("link");
-    symlink(target.getPath(), link.getPath());
+    public void testNotifiesListener() throws Exception {
+        File srcDir = tmp().createDir("a");
+        tmp().createFile("a/file");
+        File dstDir = tmp().createDir("dir");
 
-    copy(link, tmp().createDir("copied"));
+        Listener listener = mock(Listener.class);
+        ArgumentCaptor<String> srcCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> dstCaptor = ArgumentCaptor.forClass(String.class);
 
-    assertEquals(target.getPath(), readlink(tmp().get("copied/link").getPath()));
-  }
+        create(listener, asList(srcDir.getPath()), dstDir.getPath()).call();
+        verify(listener, atLeastOnce()).onCopy(srcCaptor.capture(), dstCaptor.capture());
 
-  public void testCopiesDirectory() throws Exception {
-    File srcDir = tmp().createDir("a");
-    File dstDir = tmp().createDir("dst");
-    File srcFile = new File(srcDir, "test.txt");
-    File dstFile = new File(dstDir, "a/test.txt");
-    write("Testing", srcFile, UTF_8);
+        List<String> srcExpected = asList(
+                tmp().get("a").getPath(),
+                tmp().get("a/file").getPath()
+        );
+        List<String> dstExpected = asList(
+                tmp().get("dir/a").getPath(),
+                tmp().get("dir/a/file").getPath()
+        );
+        List<String> srcActual = srcCaptor.getAllValues();
+        List<String> dstActual = dstCaptor.getAllValues();
 
-    copy(srcDir, dstDir);
-    assertEquals("Testing", Files.toString(srcFile, UTF_8));
-    assertEquals("Testing", Files.toString(dstFile, UTF_8));
-  }
+        assertEquals(srcExpected, srcActual);
+        assertEquals(dstExpected, dstActual);
+    }
 
-  public void testCopiesEmptyFile() throws Exception {
-    File srcFile = tmp().createFile("empty");
-    File dstDir = tmp().createDir("dst");
+    public void testCopiesSymlink() throws Exception {
+        File target = tmp().createFile("target");
+        File link = tmp().get("link");
+        symlink(target.getPath(), link.getPath());
 
-    copy(srcFile, dstDir);
-    assertTrue(tmp().get("dst/empty").exists());
-  }
+        copy(link, tmp().createDir("copied"));
 
-  public void testCopiesFile() throws Exception {
-    File srcFile = tmp().createFile("test.txt");
-    File dstDir = tmp().createDir("dst");
-    File dstFile = new File(dstDir, "test.txt");
-    write("Testing", srcFile, UTF_8);
+        assertEquals(target.getPath(), readlink(tmp().get("copied/link").getPath()));
+    }
 
-    copy(srcFile, dstDir);
-    assertEquals("Testing", Files.toString(srcFile, UTF_8));
-    assertEquals("Testing", Files.toString(dstFile, UTF_8));
-  }
+    public void testCopiesDirectory() throws Exception {
+        File srcDir = tmp().createDir("a");
+        File dstDir = tmp().createDir("dst");
+        File srcFile = new File(srcDir, "test.txt");
+        File dstFile = new File(dstDir, "a/test.txt");
+        write("Testing", srcFile, UTF_8);
 
-  private void copy(File src, File dstDir)
-      throws IOException, InterruptedException {
-    create(asList(src.getPath()), dstDir.getPath()).call();
-  }
+        copy(srcDir, dstDir);
+        assertEquals("Testing", Files.toString(srcFile, UTF_8));
+        assertEquals("Testing", Files.toString(dstFile, UTF_8));
+    }
 
-  @Override protected Copy create(Iterable<String> sources, String dstDir) {
-    return create(NULL_LISTENER, sources, dstDir);
-  }
+    public void testCopiesEmptyFile() throws Exception {
+        File srcFile = tmp().createFile("empty");
+        File dstDir = tmp().createDir("dst");
 
-  private Copy create(Listener listener, Iterable<String> sources, String dstDir) {
-    return new Copy(listener, sources, dstDir);
-  }
+        copy(srcFile, dstDir);
+        assertTrue(tmp().get("dst/empty").exists());
+    }
+
+    public void testCopiesFile() throws Exception {
+        File srcFile = tmp().createFile("test.txt");
+        File dstDir = tmp().createDir("dst");
+        File dstFile = new File(dstDir, "test.txt");
+        write("Testing", srcFile, UTF_8);
+
+        copy(srcFile, dstDir);
+        assertEquals("Testing", Files.toString(srcFile, UTF_8));
+        assertEquals("Testing", Files.toString(dstFile, UTF_8));
+    }
+
+    private void copy(File src, File dstDir)
+            throws IOException, InterruptedException {
+        create(asList(src.getPath()), dstDir.getPath()).call();
+    }
+
+    @Override
+    protected Copy create(Iterable<String> sources, String dstDir) {
+        return create(NULL_LISTENER, sources, dstDir);
+    }
+
+    private Copy create(Listener listener, Iterable<String> sources, String dstDir) {
+        return new Copy(listener, sources, dstDir);
+    }
 }
--- files-io-file-operations/src/main/java/l/files/io/file/operations/Copy.java
+++ files-io-file-operations/src/main/java/l/files/io/file/operations/Copy.java
@@ -32,131 +32,127 @@ public final class Copy extends Paste {
    * https://code.google.com/p/android/issues/detail?id=25460
    */
 
-  private static final Logger logger = Logger.get(Copy.class);
+    private static final Logger logger = Logger.get(Copy.class);
 
-  /* TODO check optimal size
-   * Higher the buffer, faster the copy, but will affect the overall system
-   * performance/responsiveness more.
-   * Need to keep a good balance between speed and system performance.
-   * This may be related:
-   * http://stackoverflow.com/questions/4290679/why-high-io-rate-operations-slow-everything-on-linux
-   */
-  private static final long BUFFER_SIZE = 1024 * 4;
+    /* TODO check optimal size
+     * Higher the buffer, faster the copy, but will affect the overall system
+     * performance/responsiveness more.
+     * Need to keep a good balance between speed and system performance.
+     * This may be related:
+     * http://stackoverflow.com/questions/4290679/why-high-io-rate-operations-slow-everything-on-linux
+     */
+    private static final long BUFFER_SIZE = 1024 * 4;
 
-  private final Listener listener;
+    private final Listener listener;
 
-  public Copy(Listener listener, Iterable<String> sources, String dstDir) {
-    super(sources, dstDir);
-    this.listener = checkNotNull(listener, "listener");
-  }
-
-  @Override
-  protected void paste(String from, String to, Collection<Failure> failures)
-      throws InterruptedException {
-
-    File oldRoot = new File(from);
-    File newRoot = new File(to);
-    Entry root = Entry.create(from);
-
-    for (Entry entry : DirectoryTreeTraverser.get().preOrderTraversal(root)) {
-      checkInterrupt();
-
-      FileInfo file;
-      try {
-        file = FileInfo.get(entry.path());
-      } catch (IOException e) {
-        logger.error(e);
-        failures.add(Failure.create(entry.path(), e));
-        continue;
-      }
-
-      File dst = Files.replace(new File(entry.path()), oldRoot, newRoot);
-      if (file.isSymbolicLink()) {
-        copyLink(file, dst.getPath(), failures);
-      } else if (file.isDirectory()) {
-        createDirectory(file, dst, failures);
-      } else {
-        copyFile(file, dst.getPath(), failures);
-      }
-      notifyListener(file, dst.getPath());
-    }
-  }
-
-  private void copyLink(FileInfo src, String dst, Collection<Failure> failures) {
-    try {
-      String target = readlink(src.getPath());
-      symlink(target, dst);
-      setLastModifiedDate(src, dst);
-    } catch (IOException e) {
-      failures.add(Failure.create(src.getPath(), e));
-    }
-  }
-
-  private void createDirectory(
-      FileInfo src, File dst, Collection<Failure> failures) {
-    try {
-      forceMkdir(dst);
-      setLastModifiedDate(src, dst.getPath());
-    } catch (IOException e) {
-      failures.add(Failure.create(src.getPath(), e));
-    }
-  }
-
-  private void copyFile(FileInfo src, String dst, Collection<Failure> failures)
-      throws InterruptedException {
-    checkInterrupt();
-
-    FileInputStream fis = null;
-    FileOutputStream fos = null;
-    try {
-
-      fis = new FileInputStream(src.getPath());
-      fos = new FileOutputStream(dst);
-      FileChannel input = fis.getChannel();
-      FileChannel output = fos.getChannel();
-      long size = input.size();
-      long pos = 0;
-      while (pos < size) {
-        long count = (size - pos) > BUFFER_SIZE ? BUFFER_SIZE : size - pos;
-        pos += output.transferFrom(input, pos, count);
-        notifyListener(src, dst);
-      }
-
-    } catch (IOException e) {
-      if (!new File(dst).delete()) {
-        logger.warn(e, "Failed to delete file on exception %s", dst);
-      }
-      if (e instanceof ClosedByInterruptException) {
-        throw new InterruptedException();
-      } else {
-        failures.add(Failure.create(src.getPath(), e));
-      }
-
-    } finally {
-      closeQuietly(fos);
-      closeQuietly(fis);
+    public Copy(Listener listener, Iterable<String> sources, String dstDir) {
+        super(sources, dstDir);
+        this.listener = checkNotNull(listener, "listener");
     }
 
-    setLastModifiedDate(src, dst);
-  }
+    @Override
+    protected void paste(String from, String to, Collection<Failure> failures)
+            throws InterruptedException {
 
-  private void setLastModifiedDate(FileInfo src, String dst) {
-    File dstFile = new File(dst);
-    File srcFile = new File(src.getPath());
-    if (!dstFile.setLastModified(srcFile.lastModified())) {
-      logger.warn("Failed to set last modified date on %s", dst);
+        File oldRoot = new File(from);
+        File newRoot = new File(to);
+        Entry root = Entry.create(from);
+
+        for (Entry entry : DirectoryTreeTraverser.get().preOrderTraversal(root)) {
+            checkInterrupt();
+
+            FileInfo file;
+            try {
+                file = FileInfo.get(entry.path());
+            } catch (IOException e) {
+                logger.error(e);
+                failures.add(Failure.create(entry.path(), e));
+                continue;
+            }
+
+            File dst = Files.replace(new File(entry.path()), oldRoot, newRoot);
+            if (file.isSymbolicLink()) {
+                copyLink(file, dst.getPath(), failures);
+            } else if (file.isDirectory()) {
+                createDirectory(file, dst, failures);
+            } else {
+                copyFile(file, dst.getPath(), failures);
+            }
+            notifyListener(file.getPath(), dst.getPath());
+        }
     }
-  }
 
-  private void notifyListener(FileInfo src, String dst) {
-    try {
-      listener.onCopy(src, FileInfo.get(dst));
-    } catch (IOException e) {
-      logger.warn(e);
+    private void copyLink(FileInfo src, String dst, Collection<Failure> failures) {
+        try {
+            String target = readlink(src.getPath());
+            symlink(target, dst);
+            setLastModifiedDate(src, dst);
+        } catch (IOException e) {
+            failures.add(Failure.create(src.getPath(), e));
+        }
     }
-  }
 
-  public static interface Listener {
-    void onCopy(FileInfo src, FileInfo dst);
-  }
+    private void createDirectory(
+            FileInfo src, File dst, Collection<Failure> failures) {
+        try {
+            forceMkdir(dst);
+            setLastModifiedDate(src, dst.getPath());
+        } catch (IOException e) {
+            failures.add(Failure.create(src.getPath(), e));
+        }
+    }
+
+    private void copyFile(FileInfo src, String dst, Collection<Failure> failures)
+            throws InterruptedException {
+        checkInterrupt();
+
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        try {
+
+            fis = new FileInputStream(src.getPath());
+            fos = new FileOutputStream(dst);
+            FileChannel input = fis.getChannel();
+            FileChannel output = fos.getChannel();
+            long size = input.size();
+            long pos = 0;
+            while (pos < size) {
+                long count = (size - pos) > BUFFER_SIZE ? BUFFER_SIZE : size - pos;
+                pos += output.transferFrom(input, pos, count);
+                notifyListener(src.getPath(), dst);
+            }
+
+        } catch (IOException e) {
+            if (!new File(dst).delete()) {
+                logger.warn(e, "Failed to delete file on exception %s", dst);
+            }
+            if (e instanceof ClosedByInterruptException) {
+                throw new InterruptedException();
+            } else {
+                failures.add(Failure.create(src.getPath(), e));
+            }
+
+        } finally {
+            closeQuietly(fos);
+            closeQuietly(fis);
+        }
+
+        setLastModifiedDate(src, dst);
+    }
+
+    private void setLastModifiedDate(FileInfo src, String dst) {
+        File dstFile = new File(dst);
+        File srcFile = new File(src.getPath());
+        if (!dstFile.setLastModified(srcFile.lastModified())) {
+            logger.warn("Failed to set last modified date on %s", dst);
+        }
+    }
+
+    private void notifyListener(String src, String dst) {
+        listener.onCopy(src, dst);
+    }
+
+    public static interface Listener {
+        void onCopy(String src, String dst);
+    }
 }
