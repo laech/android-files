d42dc8e14d0cfb95d06865250c42649ad7f89c39
Lae <llaechen@gmail.com>
2014-07-12 15:39:42 +1200

    Add move support to provider.

--- files-provider/src/androidTest/java/l/files/provider/FilesContractTest.java
+++ files-provider/src/androidTest/java/l/files/provider/FilesContractTest.java
@@ -8,54 +8,67 @@ import l.files.common.testing.FileBaseTest;
 
 import static java.lang.Thread.sleep;
 import static java.util.Arrays.asList;
+import static l.files.provider.FilesContract.copy;
+import static l.files.provider.FilesContract.delete;
 import static l.files.provider.FilesContract.getFileLocation;
+import static l.files.provider.FilesContract.move;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public final class FilesContractTest extends FileBaseTest {
 
-    public void testDeletesFile() throws Exception {
-        File file = tmp().createFile("a");
-        String location = getFileLocation(file);
+  public void testMovesFile() throws Exception {
+    File srcFile = tmp().createFile("a");
+    File dstDir = tmp().createDir("dst");
 
-        FilesContract.delete(getContext(), asList(location));
+    move(getContext(), asList(getFileLocation(srcFile)), getFileLocation(dstDir));
 
-        waitForFileToNotExist(file);
+    waitForFileToNotExist(srcFile);
+    assertThat(new File(dstDir, srcFile.getName()).exists(), is(true));
+  }
+
+  public void testDeletesFile() throws Exception {
+    File file = tmp().createFile("a");
+    String location = getFileLocation(file);
+
+    delete(getContext(), asList(location));
+
+    waitForFileToNotExist(file);
+  }
+
+  public void testCopiesFile() throws Exception {
+    tmp().createFile("a/b");
+    String srcLocation = getFileLocation(tmp().get("a"));
+    String dstLocation = getFileLocation(tmp().createDir("1"));
+
+    copy(getContext(), asList(srcLocation), dstLocation);
+
+    waitForFileToExist(tmp().get("1/a/b"));
+    assertTrue(tmp().get("a/b").exists());
+  }
+
+  private void waitForFileToExist(final File file) throws InterruptedException {
+    waitFor(new Supplier<Boolean>() {
+      @Override public Boolean get() {
+        return file.exists();
+      }
+    });
+  }
+
+  private void waitForFileToNotExist(final File file) throws InterruptedException {
+    waitFor(new Supplier<Boolean>() {
+      @Override public Boolean get() {
+        return !file.exists();
+      }
+    });
+  }
+
+  private void waitFor(Supplier<Boolean> success) throws InterruptedException {
+    for (int i = 0; !success.get(); i++) {
+      sleep(50);
+      if (i >= 9) {
+        fail();
+      }
     }
-
-    public void testCopiesFile() throws Exception {
-        tmp().createFile("a/b");
-        String srcLocation = getFileLocation(tmp().get("a"));
-        String dstLocation = getFileLocation(tmp().createDir("1"));
-
-        FilesContract.copy(getContext(), asList(srcLocation), dstLocation);
-
-        waitForFileToExist(tmp().get("1/a/b"));
-        assertTrue(tmp().get("a/b").exists());
-    }
-
-    private void waitForFileToExist(final File file) throws InterruptedException {
-        waitFor(new Supplier<Boolean>() {
-            @Override
-            public Boolean get() {
-                return file.exists();
-            }
-        });
-    }
-
-    private void waitForFileToNotExist(final File file) throws InterruptedException {
-        waitFor(new Supplier<Boolean>() {
-            @Override
-            public Boolean get() {
-                return !file.exists();
-            }
-        });
-    }
-
-    private void waitFor(Supplier<Boolean> success) throws InterruptedException {
-        for (int i = 0; !success.get(); i++) {
-            sleep(50);
-            if (i >= 9) {
-                fail();
-            }
-        }
-    }
+  }
 }
--- files-provider/src/main/java/l/files/provider/FilesProvider.java
+++ files-provider/src/main/java/l/files/provider/FilesProvider.java
@@ -58,262 +58,246 @@ import static l.files.provider.FilesContract.newMatcher;
 
 public final class FilesProvider extends ContentProvider {
 
-    private static final Logger logger = Logger.get(FilesProvider.class);
+  private static final Logger logger = Logger.get(FilesProvider.class);
 
-    private static final String[] DEFAULT_COLUMNS = {
-            FileInfo.LOCATION,
-            FileInfo.NAME,
-            FileInfo.SIZE,
-            FileInfo.READABLE,
-            FileInfo.WRITABLE,
-            FileInfo.MIME,
-            FileInfo.MODIFIED,
-            FileInfo.HIDDEN,
-    };
+  private static final String[] DEFAULT_COLUMNS = {
+      FileInfo.LOCATION,
+      FileInfo.NAME,
+      FileInfo.SIZE,
+      FileInfo.READABLE,
+      FileInfo.WRITABLE,
+      FileInfo.MIME,
+      FileInfo.MODIFIED,
+      FileInfo.HIDDEN,
+  };
 
-    private UriMatcher matcher;
-    private FilesCache helper;
+  private UriMatcher matcher;
+  private FilesCache helper;
 
-    @Override
-    public boolean onCreate() {
-        matcher = newMatcher(getContext());
-        helper = new FilesCache(getContext(), WatchService.get());
-        return true;
-    }
+  @Override public boolean onCreate() {
+    matcher = newMatcher(getContext());
+    helper = new FilesCache(getContext(), WatchService.get());
+    return true;
+  }
 
-    @Override
-    public String getType(Uri uri) {
-        switch (matcher.match(uri)) {
-            case MATCH_FILES_LOCATION:
-                String location = getFileLocation(uri);
-                File file = new File(URI.create(location));
-                if (file.isDirectory()) {
-                    return MIME_DIR;
-                }
-                try {
-                    return detectMime(file);
-                } catch (IOException e) {
-                    logger.warn(e);
-                    return null;
-                }
+  @Override public String getType(Uri uri) {
+    switch (matcher.match(uri)) {
+      case MATCH_FILES_LOCATION:
+        String location = getFileLocation(uri);
+        File file = new File(URI.create(location));
+        if (file.isDirectory()) {
+          return MIME_DIR;
         }
-        throw new UnsupportedOperationException("Unsupported Uri: " + uri);
-    }
-
-    private String detectMime(File file) throws IOException {
-        long start = 0;
-        if (DEBUG) {
-            start = SystemClock.elapsedRealtime();
-        }
-
-        String mime = TikaHolder.TIKA.detect(file);
-
-        if (DEBUG) {
-            long end = SystemClock.elapsedRealtime();
-            logger.debug("Detected %s in %s ms: %s", mime, (end - start), file);
-        }
-
-        return mime;
-    }
-
-    @Override
-    public ParcelFileDescriptor openFile(Uri uri, String mode) throws FileNotFoundException {
-        switch (matcher.match(uri)) {
-            case MATCH_FILES_LOCATION:
-                File file = new File(URI.create(getFileLocation(uri)));
-                return ParcelFileDescriptor.open(file, MODE_READ_ONLY);
-        }
-        return super.openFile(uri, mode);
-    }
-
-    @Override
-    public Cursor query(
-            Uri uri,
-            String[] projection,
-            String selection,
-            String[] selectionArgs,
-            String sortOrder) {
-
-        if (projection == null) {
-            projection = DEFAULT_COLUMNS;
-        }
-
-        switch (matcher.match(uri)) {
-            case MATCH_SUGGESTION:
-                return querySuggestion(uri, projection, sortOrder);
-            case MATCH_FILES_LOCATION:
-                return queryFile(uri, projection, sortOrder);
-            case MATCH_FILES_LOCATION_CHILDREN:
-                return queryFiles(uri, projection, sortOrder);
-            case MATCH_HIERARCHY:
-                return queryHierarchy(uri, projection, sortOrder);
-            case MATCH_SELECTION:
-                return querySelection(uri, projection, sortOrder);
-            default:
-                throw new UnsupportedOperationException("Unsupported Uri: " + uri);
+        try {
+          return detectMime(file);
+        } catch (IOException e) {
+          logger.warn(e);
+          return null;
         }
     }
+    throw new UnsupportedOperationException("Unsupported Uri: " + uri);
+  }
 
-    private Cursor queryHierarchy(Uri uri, String[] projection, String sortOrder) {
-        File file = normalize(new File(URI.create(getHierarchyFileLocation(uri))));
-        List<File> files = newArrayList();
-        while (file != null) {
-            files.add(file);
-            file = file.getParentFile();
-        }
-        reverse(files);
-        File[] fileArray = files.toArray(new File[files.size()]);
-        return newFileCursor(uri, projection, sortOrder, fileArray);
+  private String detectMime(File file) throws IOException {
+    long start = 0;
+    if (DEBUG) {
+      start = SystemClock.elapsedRealtime();
     }
 
-    private Cursor queryFile(Uri uri, String[] projection, String sortOrder) {
+    String mime = TikaHolder.TIKA.detect(file);
+
+    if (DEBUG) {
+      long end = SystemClock.elapsedRealtime();
+      logger.debug("Detected %s in %s ms: %s", mime, (end - start), file);
+    }
+
+    return mime;
+  }
+
+  @Override public ParcelFileDescriptor openFile(Uri uri, String mode) throws FileNotFoundException {
+    switch (matcher.match(uri)) {
+      case MATCH_FILES_LOCATION:
         File file = new File(URI.create(getFileLocation(uri)));
-        return newFileCursor(uri, projection, sortOrder, file);
+        return ParcelFileDescriptor.open(file, MODE_READ_ONLY);
+    }
+    return super.openFile(uri, mode);
+  }
+
+  @Override public Cursor query(
+      Uri uri,
+      String[] projection,
+      String selection,
+      String[] selectionArgs,
+      String sortOrder) {
+
+    if (projection == null) {
+      projection = DEFAULT_COLUMNS;
     }
 
-    private Cursor querySuggestion(Uri uri, String[] projection, String sortOrder) {
-        File parent = new File(URI.create(getSuggestionParentUri(uri)));
-        String name = getSuggestionBasename(uri);
-        File file = new File(parent, name);
-        for (int i = 2; file.exists(); i++) {
-            file = new File(parent, name + " " + i);
-        }
-        return newFileCursor(uri, projection, sortOrder, file);
-    }
-
-    private Cursor queryFiles(Uri uri, String[] projection, String sortOrder) {
-        FileData[] data = helper.get(uri, null);
-        return newFileCursor(uri, projection, sortOrder, data);
-    }
-
-    private Cursor querySelection(Uri uri, String[] projection, String sortOrder) {
-        List<String> selection = uri.getQueryParameters(PARAM_SELECTION);
-        File[] files = new File[selection.size()];
-        for (int i = 0; i < files.length; i++) {
-            files[i] = new File(URI.create(selection.get(i)));
-        }
-
-        return newFileCursor(uri, projection, sortOrder, files);
-    }
-
-    @Override
-    public Bundle call(String method, String arg, Bundle extras) {
-        switch (method) {
-            case METHOD_RENAME:
-                return callRename(extras);
-            case METHOD_DELETE:
-                return callDelete(extras);
-            case METHOD_COPY:
-                return callCopy(extras);
-            case METHOD_CUT:
-                return callCut(extras);
-        }
-        return super.call(method, arg, extras);
-    }
-
-    private Bundle callCut(Bundle extras) {
-        Set<File> files = toFilesSet(extras.getStringArray(EXTRA_FILE_LOCATIONS));
-        File destination = new File(URI.create(extras.getString(EXTRA_DESTINATION_LOCATION)));
-//    MoveService.start(getContext(), files, destination);
-        return Bundle.EMPTY;
-    }
-
-    private Bundle callCopy(Bundle extras) {
-        String[] srcPaths = toFilePaths(extras.getStringArray(EXTRA_FILE_LOCATIONS));
-        String dstPath = new File(URI.create(extras.getString(EXTRA_DESTINATION_LOCATION))).getPath();
-        OperationService.copy(getContext(), asList(srcPaths), dstPath);
-        return Bundle.EMPTY;
-    }
-
-    private Bundle callRename(Bundle extras) {
-        File from = new File(URI.create(extras.getString(EXTRA_FILE_LOCATION)));
-        File to = new File(from.getParent(), extras.getString(EXTRA_NEW_NAME));
-        Bundle out = new Bundle(1);
-        out.putBoolean(EXTRA_RESULT, from.renameTo(to));
-        return out;
-    }
-
-    private Bundle callDelete(Bundle extras) {
-        String[] paths = toFilePaths(extras.getStringArray(EXTRA_FILE_LOCATIONS));
-        OperationService.delete(getContext(), paths);
-        return Bundle.EMPTY;
-    }
-
-    private String[] toFilePaths(String[] fileLocations) {
-        Set<String> paths = newHashSetWithExpectedSize(fileLocations.length);
-        for (String location : fileLocations) {
-            paths.add(toFilePath(location));
-        }
-        return paths.toArray(new String[paths.size()]);
-    }
-
-    private String toFilePath(String location) {
-        return new File(URI.create(location)).getPath();
-    }
-
-    private Set<File> toFilesSet(String[] fileLocations) {
-        Set<File> files = newHashSetWithExpectedSize(fileLocations.length);
-        for (String location : fileLocations) {
-            files.add(new File(URI.create(location)));
-        }
-        return files;
-    }
-
-    @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        switch (matcher.match(uri)) {
-            case MATCH_FILES_LOCATION:
-                return insertDirectory(uri);
-        }
+    switch (matcher.match(uri)) {
+      case MATCH_SUGGESTION:
+        return querySuggestion(uri, projection, sortOrder);
+      case MATCH_FILES_LOCATION:
+        return queryFile(uri, projection, sortOrder);
+      case MATCH_FILES_LOCATION_CHILDREN:
+        return queryFiles(uri, projection, sortOrder);
+      case MATCH_HIERARCHY:
+        return queryHierarchy(uri, projection, sortOrder);
+      case MATCH_SELECTION:
+        return querySelection(uri, projection, sortOrder);
+      default:
         throw new UnsupportedOperationException("Unsupported Uri: " + uri);
     }
+  }
 
-    private Uri insertDirectory(Uri uri) {
-        File file = new File(URI.create(getFileLocation(uri)));
-        return (file.mkdirs() || file.isDirectory()) ? uri : null;
+  private Cursor queryHierarchy(Uri uri, String[] projection, String sortOrder) {
+    File file = normalize(new File(URI.create(getHierarchyFileLocation(uri))));
+    List<File> files = newArrayList();
+    while (file != null) {
+      files.add(file);
+      file = file.getParentFile();
+    }
+    reverse(files);
+    File[] fileArray = files.toArray(new File[files.size()]);
+    return newFileCursor(uri, projection, sortOrder, fileArray);
+  }
+
+  private Cursor queryFile(Uri uri, String[] projection, String sortOrder) {
+    File file = new File(URI.create(getFileLocation(uri)));
+    return newFileCursor(uri, projection, sortOrder, file);
+  }
+
+  private Cursor querySuggestion(Uri uri, String[] projection, String sortOrder) {
+    File parent = new File(URI.create(getSuggestionParentUri(uri)));
+    String name = getSuggestionBasename(uri);
+    File file = new File(parent, name);
+    for (int i = 2; file.exists(); i++) {
+      file = new File(parent, name + " " + i);
+    }
+    return newFileCursor(uri, projection, sortOrder, file);
+  }
+
+  private Cursor queryFiles(Uri uri, String[] projection, String sortOrder) {
+    FileData[] data = helper.get(uri, null);
+    return newFileCursor(uri, projection, sortOrder, data);
+  }
+
+  private Cursor querySelection(Uri uri, String[] projection, String sortOrder) {
+    List<String> selection = uri.getQueryParameters(PARAM_SELECTION);
+    File[] files = new File[selection.size()];
+    for (int i = 0; i < files.length; i++) {
+      files[i] = new File(URI.create(selection.get(i)));
     }
 
-    @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        throw new UnsupportedOperationException("Unsupported Uri: " + uri);
-    }
+    return newFileCursor(uri, projection, sortOrder, files);
+  }
 
-    @Override
-    public int update(
-            Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        throw new UnsupportedOperationException("Update not supported");
+  @Override public Bundle call(String method, String arg, Bundle extras) {
+    switch (method) {
+      case METHOD_RENAME:
+        return callRename(extras);
+      case METHOD_DELETE:
+        return callDelete(extras);
+      case METHOD_COPY:
+        return callCopy(extras);
+      case METHOD_CUT:
+        return callCut(extras);
     }
+    return super.call(method, arg, extras);
+  }
 
-    private void sort(FileData[] data, String sortOrder) {
-        if (sortOrder != null) {
-            Arrays.sort(data, SortBy.valueOf(sortOrder));
-        }
-    }
+  private Bundle callCut(Bundle extras) {
+    String[] srcPaths = toFilePaths(extras.getStringArray(EXTRA_FILE_LOCATIONS));
+    String dstPath = new File(URI.create(extras.getString(EXTRA_DESTINATION_LOCATION))).getPath();
+    OperationService.move(getContext(), asList(srcPaths), dstPath);
+    return Bundle.EMPTY;
+  }
 
-    private Cursor newFileCursor(Uri uri, String[] projection, String sortOrder, File... files) {
-        List<FileData> stats = newArrayListWithCapacity(files.length);
-        for (File file : files) {
-            try {
-                stats.add(FileData.get(Path.from(file)));
-            } catch (IOException e) {
-                logger.warn(e);
-            }
-        }
-        return newFileCursor(uri, projection, sortOrder, stats.toArray(new FileData[stats.size()]));
-    }
+  private Bundle callCopy(Bundle extras) {
+    String[] srcPaths = toFilePaths(extras.getStringArray(EXTRA_FILE_LOCATIONS));
+    String dstPath = new File(URI.create(extras.getString(EXTRA_DESTINATION_LOCATION))).getPath();
+    OperationService.copy(getContext(), asList(srcPaths), dstPath);
+    return Bundle.EMPTY;
+  }
 
-    private Cursor newFileCursor(Uri uri, String[] projection, String sortOrder, FileData... files) {
-        sort(files, sortOrder);
-        Cursor c = new FileCursor(files, projection);
-        c.setNotificationUri(getContentResolver(), uri);
-        return c;
-    }
+  private Bundle callRename(Bundle extras) {
+    File from = new File(URI.create(extras.getString(EXTRA_FILE_LOCATION)));
+    File to = new File(from.getParent(), extras.getString(EXTRA_NEW_NAME));
+    Bundle out = new Bundle(1);
+    out.putBoolean(EXTRA_RESULT, from.renameTo(to));
+    return out;
+  }
 
-    private ContentResolver getContentResolver() {
-        return getContext().getContentResolver();
-    }
+  private Bundle callDelete(Bundle extras) {
+    String[] paths = toFilePaths(extras.getStringArray(EXTRA_FILE_LOCATIONS));
+    OperationService.delete(getContext(), paths);
+    return Bundle.EMPTY;
+  }
 
-    private static class TikaHolder {
-        static final Tika TIKA = new Tika();
+  private String[] toFilePaths(String[] fileLocations) {
+    Set<String> paths = newHashSetWithExpectedSize(fileLocations.length);
+    for (String location : fileLocations) {
+      paths.add(toFilePath(location));
     }
+    return paths.toArray(new String[paths.size()]);
+  }
+
+  private String toFilePath(String location) {
+    return new File(URI.create(location)).getPath();
+  }
+
+  @Override public Uri insert(Uri uri, ContentValues values) {
+    switch (matcher.match(uri)) {
+      case MATCH_FILES_LOCATION:
+        return insertDirectory(uri);
+    }
+    throw new UnsupportedOperationException("Unsupported Uri: " + uri);
+  }
+
+  private Uri insertDirectory(Uri uri) {
+    File file = new File(URI.create(getFileLocation(uri)));
+    return (file.mkdirs() || file.isDirectory()) ? uri : null;
+  }
+
+  @Override public int delete(Uri uri, String selection, String[] selectionArgs) {
+    throw new UnsupportedOperationException("Unsupported Uri: " + uri);
+  }
+
+  @Override public int update(
+      Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+    throw new UnsupportedOperationException("Update not supported");
+  }
+
+  private void sort(FileData[] data, String sortOrder) {
+    if (sortOrder != null) {
+      Arrays.sort(data, SortBy.valueOf(sortOrder));
+    }
+  }
+
+  private Cursor newFileCursor(Uri uri, String[] projection, String sortOrder, File... files) {
+    List<FileData> stats = newArrayListWithCapacity(files.length);
+    for (File file : files) {
+      try {
+        stats.add(FileData.get(Path.from(file)));
+      } catch (IOException e) {
+        logger.warn(e);
+      }
+    }
+    return newFileCursor(uri, projection, sortOrder, stats.toArray(new FileData[stats.size()]));
+  }
+
+  private Cursor newFileCursor(Uri uri, String[] projection, String sortOrder, FileData... files) {
+    sort(files, sortOrder);
+    Cursor c = new FileCursor(files, projection);
+    c.setNotificationUri(getContentResolver(), uri);
+    return c;
+  }
+
+  private ContentResolver getContentResolver() {
+    return getContext().getContentResolver();
+  }
+
+  private static class TikaHolder {
+    static final Tika TIKA = new Tika();
+  }
 }
