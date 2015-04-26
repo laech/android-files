0267353bf9c5d8bbd80300a5acdcd13965f9911d
Lae <llaechen@gmail.com>
2015-04-27 08:53:17 +1200

    Delete unnecessary things.

--- files-app/src/main/java/l/files/fs/local/Unistd.java
+++ files-app/src/main/java/l/files/fs/local/Unistd.java
@@ -5,31 +5,12 @@ package l.files.fs.local;
  */
 final class Unistd extends Native {
 
-  public static final int R_OK = 4;  /* Read */
-  public static final int W_OK = 2;  /* Write */
-  public static final int X_OK = 1;  /* Execute */
-  public static final int F_OK = 0;  /* Existence */
+    private Unistd() {
+    }
 
-  private Unistd() {}
-
-  /**
-   * @see <a href="http://pubs.opengroup.org/onlinepubs/7908799/xsh/access.html">access()</a>
-   */
-  public static native void access(String path, int mode) throws ErrnoException;
-
-  /**
-   * @see <a href="http://pubs.opengroup.org/onlinepubs/000095399/functions/symlink.html">symlink()</a>
-   */
-  public static native void symlink(String target, String link) throws ErrnoException;
-
-  /**
-   * @see <a href="http://pubs.opengroup.org/onlinepubs/000095399/functions/readlink.html">readlink()</a>
-   */
-  public static native String readlink(String link) throws ErrnoException;
-
-  /**
-   * @see <a href="http://pubs.opengroup.org/onlinepubs/7908799/xsh/close.html">close</a>
-   */
-  public static native void close(int fd) throws ErrnoException;
+    /**
+     * @see <a href="http://pubs.opengroup.org/onlinepubs/7908799/xsh/close.html">close</a>
+     */
+    public static native void close(int fd) throws ErrnoException;
 
 }
