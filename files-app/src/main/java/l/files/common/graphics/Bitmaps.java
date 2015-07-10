a8210adac1ea386349c057853ab1843f51d3520d
Lae <llaechen@gmail.com>
2015-07-10 22:29:39 +1200

    Format.

--- files-app/src/main/java/l/files/common/graphics/Bitmaps.java
+++ files-app/src/main/java/l/files/common/graphics/Bitmaps.java
@@ -2,53 +2,46 @@ package l.files.common.graphics;
 
 import android.graphics.Bitmap;
 
+import static java.lang.Math.max;
 import static java.lang.Math.min;
 import static java.lang.Math.round;
 
-public final class Bitmaps
-{
-    private Bitmaps()
-    {
-    }
+public final class Bitmaps {
+  private Bitmaps() {
+  }
 
-    public static ScaledSize scale(
-            final Bitmap bitmap,
-            final int maxWidth,
-            final int maxHeight)
-    {
-        return scale(
-                bitmap.getWidth(),
-                bitmap.getHeight(),
-                maxWidth,
-                maxHeight);
-    }
+  public static ScaledSize scale(Bitmap bitmap, int maxWidth, int maxHeight) {
+    return scale(
+        bitmap.getWidth(),
+        bitmap.getHeight(),
+        maxWidth,
+        maxHeight);
+  }
 
-    public static ScaledSize scale(
-            final int width,
-            final int height,
-            final int maxWidth,
-            final int maxHeight)
-    {
-        checkGreaterThanZero(width);
-        checkGreaterThanZero(height);
-        checkGreaterThanZero(maxWidth);
-        checkGreaterThanZero(maxHeight);
+  public static ScaledSize scale(
+      int width,
+      int height,
+      int maxWidth,
+      int maxHeight) {
 
-        final float widthRatio = maxWidth / (float) width;
-        final float heightRatio = maxHeight / (float) height;
-        final float scale = min(widthRatio, heightRatio);
-        final int scaledWith = round(width * scale);
-        final int scaledHeight = round(height * scale);
-        return ScaledSize.of(width, height, scaledWith, scaledHeight, scale);
-    }
+    checkGreaterThanZero(width);
+    checkGreaterThanZero(height);
+    checkGreaterThanZero(maxWidth);
+    checkGreaterThanZero(maxHeight);
 
-    private static void checkGreaterThanZero(final int value)
-    {
-        if (value <= 0)
-        {
-            throw new IllegalArgumentException(
-                    "Must be greater than zero, got " + value);
-        }
+    float widthRatio = maxWidth / (float) width;
+    float heightRatio = maxHeight / (float) height;
+    float scale = min(widthRatio, heightRatio);
+    int scaledWith = max(round(width * scale), 1);
+    int scaledHeight = max(round(height * scale), 1);
+    return ScaledSize.of(width, height, scaledWith, scaledHeight, scale);
+  }
+
+  private static void checkGreaterThanZero(int value) {
+    if (value <= 0) {
+      throw new IllegalArgumentException(
+          "Must be greater than zero, got " + value);
     }
+  }
 
 }
