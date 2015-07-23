7385556c300e01429431498d2c3c624ce9c0cc55
Lae <llaechen@gmail.com>
2015-07-23 22:01:47 +1200

    Remove dependency.

--- files-app/src/main/java/l/files/ui/newdir/NewDirFragment.java
+++ files-app/src/main/java/l/files/ui/newdir/NewDirFragment.java
@@ -1,6 +1,7 @@
 package l.files.ui.newdir;
 
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.EditText;
 
@@ -9,158 +10,117 @@ import java.io.IOException;
 import l.files.R;
 import l.files.fs.Resource;
 import l.files.ui.FileCreationFragment;
-import rx.Subscriber;
-import rx.Subscription;
-import rx.functions.Action1;
-import rx.functions.Actions;
-import rx.functions.Func1;
-import rx.subscriptions.Subscriptions;
 
+import static android.os.AsyncTask.THREAD_POOL_EXECUTOR;
 import static l.files.fs.LinkOption.NOFOLLOW;
 import static l.files.ui.IOExceptions.message;
-import static rx.Observable.just;
-import static rx.android.app.AppObservable.bindFragment;
-import static rx.schedulers.Schedulers.io;
 
-public final class NewDirFragment extends FileCreationFragment
-{
-    public static final String TAG = NewDirFragment.class.getSimpleName();
+public final class NewDirFragment extends FileCreationFragment {
 
-    static NewDirFragment create(final Resource resource)
-    {
-        final Bundle bundle = new Bundle(1);
-        bundle.putParcelable(ARG_PARENT_RESOURCE, resource);
+  public static final String TAG = NewDirFragment.class.getSimpleName();
 
-        final NewDirFragment fragment = new NewDirFragment();
-        fragment.setArguments(bundle);
-        return fragment;
+  static NewDirFragment create(Resource resource) {
+    Bundle bundle = new Bundle(1);
+    bundle.putParcelable(ARG_PARENT_RESOURCE, resource);
+
+    NewDirFragment fragment = new NewDirFragment();
+    fragment.setArguments(bundle);
+    return fragment;
+  }
+
+  private AsyncTask<?, ?, ?> suggestion;
+  private AsyncTask<?, ?, ?> creation;
+
+  @Override public void onDestroy() {
+    super.onDestroy();
+
+    if (suggestion != null) {
+      suggestion.cancel(true);
     }
-
-    private Subscription suggestion = Subscriptions.empty();
-    private Subscription creation = Subscriptions.empty();
-
-    @Override
-    public void onDestroy()
-    {
-        super.onDestroy();
-        suggestion.unsubscribe();
-        creation.unsubscribe();
+    if (creation != null) {
+      creation.cancel(true);
     }
+  }
 
-    @Override
-    public void onActivityCreated(final Bundle savedInstanceState)
-    {
-        super.onActivityCreated(savedInstanceState);
-        suggestName();
-    }
+  @Override public void onActivityCreated(Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    suggestName();
+  }
 
-    private void suggestName()
-    {
-        final String name = getString(R.string.untitled_dir);
-        final Resource base = parent().resolve(name);
-        suggestion = bindFragment(this, just(base))
-                .subscribeOn(io())
-                .map(new SuggestName())
-                .subscribe(new SetName());
-    }
+  private void suggestName() {
+    String name = getString(R.string.untitled_dir);
+    Resource base = parent().resolve(name);
+    suggestion = new SuggestName().executeOnExecutor(THREAD_POOL_EXECUTOR, base);
+  }
 
-    private static final class SuggestName implements Func1<Resource, Resource>
-    {
-        @Override
-        public Resource call(final Resource base)
-        {
-            final String baseName = base.name().toString();
-            final Resource parent = base.parent();
-            assert parent != null;
-            Resource resource = base;
-            try
-            {
-                for (int i = 2; resource.exists(NOFOLLOW); i++)
-                {
-                    resource = parent.resolve(baseName + " " + i);
-                }
-                return resource;
-            }
-            catch (final IOException e)
-            {
-                throw new RuntimeException(e);
-            }
+  private class SuggestName extends AsyncTask<Resource, Void, Resource> {
+
+    @Override protected Resource doInBackground(Resource... params) {
+      Resource base = params[0];
+      String baseName = base.name().toString();
+      Resource parent = base.parent();
+      assert parent != null;
+      Resource resource = base;
+      try {
+        for (int i = 2; resource.exists(NOFOLLOW); i++) {
+          if (isCancelled()) {
+            return null;
+          }
+          resource = parent.resolve(baseName + " " + i);
         }
+        return resource;
+      } catch (IOException e) {
+        return null;
+      }
     }
 
-    private final class SetName extends Subscriber<Resource>
-    {
-        @Override
-        public void onCompleted()
-        {
-        }
-
-        @Override
-        public void onError(final Throwable e)
-        {
-            set("");
-        }
-
-        @Override
-        public void onNext(final Resource resource)
-        {
-            set(resource.name().toString());
-        }
-
-        private void set(final String name)
-        {
-            final EditText field = getFilenameField();
-            final boolean notChanged = field.getText().length() == 0;
-            if (notChanged)
-            {
-                field.setText(name);
-                field.selectAll();
-            }
-        }
+    @Override protected void onPostExecute(Resource result) {
+      super.onPostExecute(result);
+      if (result == null) {
+        set("");
+      } else {
+        set(result.name().toString());
+      }
     }
 
-    @Override
-    public void onClick(final DialogInterface dialog, final int which)
-    {
-        createDir(parent().resolve(getFilename()));
+    private void set(String name) {
+      EditText field = getFilenameField();
+      boolean notChanged = field.getText().length() == 0;
+      if (notChanged) {
+        field.setText(name);
+        field.selectAll();
+      }
+    }
+  }
+
+  @Override public void onClick(DialogInterface dialog, int which) {
+    createDir(parent().resolve(getFilename()));
+  }
+
+  private void createDir(Resource dir) {
+    creation = new CreateDir().executeOnExecutor(THREAD_POOL_EXECUTOR, dir);
+  }
+
+  private class CreateDir extends AsyncTask<Resource, Void, IOException> {
+
+    @Override protected IOException doInBackground(Resource... params) {
+      try {
+        params[0].createDirectory();
+        return null;
+      } catch (IOException e) {
+        return e;
+      }
     }
 
-    private void createDir(final Resource dir)
-    {
-        creation = bindFragment(this, just(dir))
-                .subscribeOn(io())
-                .doOnNext(new CreateDir())
-                .subscribe(Actions.empty(), new CreateFailed());
+    @Override protected void onPostExecute(IOException e) {
+      super.onPostExecute(e);
+      if (e != null) {
+        toaster.apply(message(e));
+      }
     }
+  }
 
-    private static final class CreateDir implements Action1<Resource>
-    {
-        @Override
-        public void call(final Resource dir)
-        {
-            try
-            {
-                dir.createDirectory();
-            }
-            catch (final IOException e)
-            {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    private final class CreateFailed implements Action1<Throwable>
-    {
-        @Override
-        public void call(final Throwable throwable)
-        {
-            toaster.apply(message((IOException) throwable.getCause()));
-        }
-    }
-
-    @Override
-    protected int getTitleResourceId()
-    {
-        return R.string.new_dir;
-    }
+  @Override protected int getTitleResourceId() {
+    return R.string.new_dir;
+  }
 }
--- files-app/src/main/java/l/files/ui/rename/RenameFragment.java
+++ files-app/src/main/java/l/files/ui/rename/RenameFragment.java
@@ -1,6 +1,7 @@
 package l.files.ui.rename;
 
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Pair;
 import android.widget.EditText;
@@ -13,183 +14,129 @@ import l.files.fs.Stat;
 import l.files.operations.Events;
 import l.files.ui.CloseActionModeRequest;
 import l.files.ui.FileCreationFragment;
-import rx.Subscriber;
-import rx.Subscription;
-import rx.functions.Action1;
-import rx.functions.Actions;
-import rx.functions.Func1;
-import rx.subscriptions.Subscriptions;
 
+import static android.os.AsyncTask.THREAD_POOL_EXECUTOR;
 import static l.files.fs.LinkOption.NOFOLLOW;
 import static l.files.ui.IOExceptions.message;
-import static rx.Observable.just;
-import static rx.android.app.AppObservable.bindFragment;
-import static rx.schedulers.Schedulers.io;
 
-public final class RenameFragment extends FileCreationFragment
-{
+public final class RenameFragment extends FileCreationFragment {
 
-    public static final String TAG = RenameFragment.class.getSimpleName();
+  public static final String TAG = RenameFragment.class.getSimpleName();
 
-    private static final String ARG_RESOURCE = "resource";
+  private static final String ARG_RESOURCE = "resource";
 
-    static RenameFragment create(final Resource resource)
-    {
-        final Bundle args = new Bundle(2);
-        args.putParcelable(ARG_PARENT_RESOURCE, resource.parent());
-        args.putParcelable(ARG_RESOURCE, resource);
+  static RenameFragment create(Resource resource) {
+    Bundle args = new Bundle(2);
+    args.putParcelable(ARG_PARENT_RESOURCE, resource.parent());
+    args.putParcelable(ARG_RESOURCE, resource);
 
-        final RenameFragment fragment = new RenameFragment();
-        fragment.setArguments(args);
-        return fragment;
+    RenameFragment fragment = new RenameFragment();
+    fragment.setArguments(args);
+    return fragment;
+  }
+
+  private AsyncTask<?, ?, ?> highlight;
+  private AsyncTask<?, ?, ?> rename;
+
+  @Override public void onDestroy() {
+    super.onDestroy();
+
+    if (highlight != null) {
+      highlight.cancel(true);
     }
+    if (rename != null) {
+      rename.cancel(true);
+    }
+  }
 
-    private Subscription highlight = Subscriptions.empty();
-    private Subscription rename = Subscriptions.empty();
+  @Override public void onStart() {
+    super.onStart();
+    highlight();
+  }
+
+  private Resource resource() {
+    return getArguments().getParcelable(ARG_RESOURCE);
+  }
+
+  private void highlight() {
+    if (getFilename().isEmpty()) {
+      highlight = new Highlight()
+          .executeOnExecutor(THREAD_POOL_EXECUTOR, resource());
+    }
+  }
+
+  private class Highlight extends AsyncTask<Resource, Void, Pair<Resource, Stat>> {
 
     @Override
-    public void onDestroy()
-    {
-        super.onDestroy();
-        highlight.unsubscribe();
-        rename.unsubscribe();
+    protected Pair<Resource, Stat> doInBackground(Resource... params) {
+      Resource resource = params[0];
+      try {
+        return Pair.create(resource, resource.stat(NOFOLLOW));
+      } catch (IOException e) {
+        return null;
+      }
     }
 
-    @Override
-    public void onStart()
-    {
-        super.onStart();
-        highlight();
-    }
-
-    private Resource resource()
-    {
-        return getArguments().getParcelable(ARG_RESOURCE);
-    }
-
-    private void highlight()
-    {
-        if (getFilename().isEmpty())
-        {
-            highlight = bindFragment(this, just(resource()))
-                    .subscribeOn(io())
-                    .map(new Status())
-                    .subscribe(new Highlight());
+    @Override protected void onPostExecute(Pair<Resource, Stat> pair) {
+      super.onPostExecute(pair);
+      if (pair != null) {
+        Resource resource = pair.first;
+        Stat stat = pair.second;
+        EditText field = getFilenameField();
+        if (!getFilename().isEmpty()) {
+          return;
         }
-    }
-
-    private static final class Status
-            implements Func1<Resource, Pair<Resource, Stat>>
-    {
-        @Override
-        public Pair<Resource, Stat> call(final Resource resource)
-        {
-            try
-            {
-                return Pair.create(resource, resource.stat(NOFOLLOW));
-            }
-            catch (final IOException e)
-            {
-                throw new RuntimeException(e);
-            }
+        field.setText(resource.name());
+        if (stat.isDirectory()) {
+          field.selectAll();
+        } else {
+          field.setSelection(0, resource.name().base().length());
         }
+      }
+    }
+  }
+
+  @Override protected CharSequence getError(Resource target) {
+    if (resource().equals(target)) {
+      return null;
+    }
+    return super.getError(target);
+  }
+
+  @Override protected int getTitleResourceId() {
+    return R.string.rename;
+  }
+
+  @Override public void onClick(DialogInterface dialog, int which) {
+    rename();
+  }
+
+  private void rename() {
+    Resource dst = parent().resolve(getFilename());
+    rename = new Rename()
+        .executeOnExecutor(THREAD_POOL_EXECUTOR, resource(), dst);
+    Events.get().post(CloseActionModeRequest.INSTANCE);
+  }
+
+  private class Rename extends AsyncTask<Resource, Void, IOException> {
+
+    @Override protected IOException doInBackground(Resource... params) {
+      Resource src = params[0];
+      Resource dst = params[1];
+      try {
+        src.moveTo(dst);
+        return null;
+      } catch (IOException e) {
+        return e;
+      }
     }
 
-    private final class Highlight extends Subscriber<Pair<Resource, Stat>>
-    {
-        @Override
-        public void onCompleted()
-        {
-        }
-
-        @Override
-        public void onError(final Throwable e)
-        {
-        }
-
-        @Override
-        public void onNext(final Pair<Resource, Stat> pair)
-        {
-            final Resource resource = pair.first;
-            final Stat stat = pair.second;
-            final EditText field = getFilenameField();
-            if (!getFilename().isEmpty())
-            {
-                return;
-            }
-            field.setText(resource.name());
-            if (stat.isDirectory())
-            {
-                field.selectAll();
-            }
-            else
-            {
-                field.setSelection(0, resource.name().base().length());
-            }
-        }
+    @Override protected void onPostExecute(IOException e) {
+      super.onPostExecute(e);
+      if (e != null) {
+        toaster.apply(message(e));
+      }
     }
+  }
 
-    @Override
-    protected CharSequence getError(final Resource target)
-    {
-        if (resource().equals(target))
-        {
-            return null;
-        }
-        return super.getError(target);
-    }
-
-    @Override
-    protected int getTitleResourceId()
-    {
-        return R.string.rename;
-    }
-
-    @Override
-    public void onClick(final DialogInterface dialog, final int which)
-    {
-        rename();
-    }
-
-    private void rename()
-    {
-        final Resource dst = parent().resolve(getFilename());
-        rename = bindFragment(this, just(resource()))
-                .subscribeOn(io())
-                .doOnNext(new MoveTo(dst))
-                .subscribe(Actions.empty(), new MoveFailed());
-        Events.get().post(CloseActionModeRequest.INSTANCE);
-    }
-
-    private final static class MoveTo implements Action1<Resource>
-    {
-        private final Resource dst;
-
-        public MoveTo(final Resource dst)
-        {
-            this.dst = dst;
-        }
-
-        @Override
-        public void call(final Resource resource)
-        {
-            try
-            {
-                resource.moveTo(dst);
-            }
-            catch (final IOException e)
-            {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    private final class MoveFailed implements Action1<Throwable>
-    {
-        @Override
-        public void call(final Throwable throwable)
-        {
-            toaster.apply(message((IOException) throwable.getCause()));
-        }
-    }
 }
