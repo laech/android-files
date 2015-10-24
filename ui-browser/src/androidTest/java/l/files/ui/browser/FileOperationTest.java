b75dcebe44462ee8382a5b05a2aed09b6608f5ee
Lae <llaechen@gmail.com>
2015-10-24 15:55:54 +1300

    Format.

--- ui-browser/src/androidTest/java/l/files/ui/browser/FileOperationTest.java
+++ ui-browser/src/androidTest/java/l/files/ui/browser/FileOperationTest.java
@@ -15,8 +15,9 @@ public final class FileOperationTest extends BaseFilesActivityTest {
     // TODO cut/delete tests
 
     public void test_copies_files() throws Exception {
-        final File a = dir().resolve("a").createFile();
-        final File d = dir().resolve("d").createDir();
+
+        File a = dir().resolve("a").createFile();
+        File d = dir().resolve("d").createDir();
 
         screen()
                 .longClick(a)
@@ -28,8 +29,9 @@ public final class FileOperationTest extends BaseFilesActivityTest {
     }
 
     public void test_copies_empty_directory() throws Exception {
-        final File c = dir().resolve("c").createDir();
-        final File d = dir().resolve("d").createDir();
+
+        File c = dir().resolve("c").createDir();
+        File d = dir().resolve("d").createDir();
 
         screen()
                 .longClick(c)
@@ -41,8 +43,9 @@ public final class FileOperationTest extends BaseFilesActivityTest {
     }
 
     public void test_copies_full_directory() throws Exception {
-        final File d = dir().resolve("d").createDir();
-        final File c = dir().resolve("c").createDir();
+
+        File d = dir().resolve("d").createDir();
+        File c = dir().resolve("c").createDir();
         c.resolve("a").createFile();
         c.resolve("b").createDir();
         c.resolve("c").createLink(c.resolve("a"));
@@ -57,8 +60,9 @@ public final class FileOperationTest extends BaseFilesActivityTest {
     }
 
     public void test_copies_link() throws Exception {
-        final File d = dir().resolve("d").createDir();
-        final File c = dir().resolve("c").createLink(dir());
+
+        File d = dir().resolve("d").createDir();
+        File c = dir().resolve("c").createLink(dir());
 
         screen()
                 .longClick(c)
@@ -70,10 +74,11 @@ public final class FileOperationTest extends BaseFilesActivityTest {
     }
 
     private boolean waitFor(
-            final File file,
-            final int time,
-            final TimeUnit unit) throws InterruptedException, IOException {
-        final long end = currentTimeMillis() + unit.toMillis(time);
+            File file,
+            int time,
+            TimeUnit unit) throws InterruptedException, IOException {
+
+        long end = currentTimeMillis() + unit.toMillis(time);
         while (currentTimeMillis() < end) {
             if (file.exists(NOFOLLOW)) {
                 return true;
@@ -85,7 +90,8 @@ public final class FileOperationTest extends BaseFilesActivityTest {
 
     public void test_paste_menu_is_disabled_inside_folder_being_copied()
             throws Exception {
-        final File dir = dir().resolve("dir").createDir();
+
+        File dir = dir().resolve("dir").createDir();
 
         screen()
                 .longClick(dir)
@@ -100,7 +106,8 @@ public final class FileOperationTest extends BaseFilesActivityTest {
 
     public void test_paste_menu_is_disabled_if_files_do_not_exist()
             throws Exception {
-        final File dir = dir().resolve("dir").createDir();
+
+        File dir = dir().resolve("dir").createDir();
 
         screen()
                 .longClick(dir)
@@ -114,7 +121,8 @@ public final class FileOperationTest extends BaseFilesActivityTest {
 
     public void test_paste_menu_is_enabled_if_some_files_do_not_exist_some_exist()
             throws Exception {
-        final File dir = dir().resolve("dir1").createDir();
+
+        File dir = dir().resolve("dir1").createDir();
         dir().resolve("dir2").createDir();
 
         screen()
