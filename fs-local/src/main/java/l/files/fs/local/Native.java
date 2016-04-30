f42708eaf6bda7a5d7dca2435987d3cfca81eb23
Lae <llaechen@gmail.com>
2016-04-30 21:14:33 +1200

    Clean up.

--- fs-local/src/main/java/l/files/fs/local/Native.java
+++ fs-local/src/main/java/l/files/fs/local/Native.java
@@ -1,20 +1,9 @@
 package l.files.fs.local;
 
-import static java.lang.System.loadLibrary;
-
 class Native {
 
-    private static boolean loaded = false;
-
     static {
-        load();
-    }
-
-    synchronized static void load() {
-        if (!loaded) {
-            loaded = true;
-            loadLibrary("fslocal");
-        }
+        System.loadLibrary("fslocal");
     }
 
 }
