5a2ee7b991ea393f75c1b9e110320bc72f54e7b4
Lae <llaechen@gmail.com>
2016-05-09 20:28:46 +1200

    Fix error message not showing.

--- fs/src/main/java/l/files/fs/IOExceptionReason.java
+++
@@ -1,7 +0,0 @@
-package l.files.fs;
-
-public interface IOExceptionReason {
-
-    String reason();
-
-}
--- ui-base/src/main/java/l/files/ui/base/fs/IOExceptions.java
+++ ui-base/src/main/java/l/files/ui/base/fs/IOExceptions.java
@@ -2,7 +2,7 @@ package l.files.ui.base.fs;
 
 import java.io.IOException;
 
-import l.files.fs.IOExceptionReason;
+import linux.ErrnoException;
 
 public final class IOExceptions {
 
@@ -11,8 +11,8 @@ public final class IOExceptions {
 
     public static String message(IOException exception) {
         Throwable cause = exception.getCause();
-        if (cause instanceof IOExceptionReason) {
-            return ((IOExceptionReason) cause).reason();
+        if (cause instanceof ErrnoException) {
+            return cause.getMessage();
         }
         return exception.getMessage();
     }
