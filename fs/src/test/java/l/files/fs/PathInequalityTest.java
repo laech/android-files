e67ee0421172dc177a71082324b5b9bc093e654b
Lae <llaechen@gmail.com>
2016-10-24 09:56:05 +1300

    Add inequality test.

---
+++ fs/src/test/java/l/files/fs/PathInequalityTest.java
@@ -0,0 +1,49 @@
+package l.files.fs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public final class PathInequalityTest {
+
+    private final String path1;
+    private final String path2;
+
+    public PathInequalityTest(String path1, String path2) {
+        this.path1 = path1;
+        this.path2 = path2;
+    }
+
+    @Parameters(name = "\"{0}\"")
+    public static Collection<Object[]> paths() {
+        return asList(new Object[][]{
+                {"", " "},
+                {"a", "b"},
+                {"a", "A"},
+                {"/a", "a"},
+                {"a", "/a"},
+                {"a/b", "/a/b"},
+                {"", "."},
+                {".", ".."},
+                {"./.", "."},
+        });
+    }
+
+    @Test
+    public void paths_are_not_equivalent() throws Exception {
+        Set<Path> paths = new HashSet<>();
+        paths.add(Path.fromString(path1));
+        paths.add(Path.fromString(path2));
+        assertEquals(2, paths.size());
+    }
+
+}
