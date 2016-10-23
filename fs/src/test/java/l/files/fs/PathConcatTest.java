7f58a93c6b526f5370425d1c4a28a2d4a21b59e5
Lae <llaechen@gmail.com>
2016-10-24 08:57:25 +1300

    Add test.

---
+++ fs/src/test/java/l/files/fs/PathConcatTest.java
@@ -0,0 +1,101 @@
+package l.files.fs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Collection;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public final class PathConcatTest {
+
+    private final String basePath;
+    private final String additionalPath;
+    private final String expectedPath;
+
+    public PathConcatTest(
+            String basePath,
+            String additionalPath,
+            String expectedPath) {
+
+        this.basePath = basePath;
+        this.additionalPath = additionalPath;
+        this.expectedPath = expectedPath;
+    }
+
+    @Parameters(name = "\"{0}\"")
+    public static Collection<Object[]> paths() {
+        return asList(new Object[][]{
+                {"", "", ""},
+                {" ", " ", " / "},
+                {"\t", "\t", "\t/\t"},
+                {"\n", "\n", "\n/\n"},
+                {"/", "/", "/"},
+                {"//", "/", "/"},
+                {".", ".", "./."},
+                {"./.", "./.", "./././."},
+                {"..", "..", "../.."},
+                {"../.", "../.", ".././../."},
+                {"../..", "../..", "../../../.."},
+                {"a", "b", "a/b"},
+                {"a", "b/", "a/b"},
+                {"a", "b//", "a/b"},
+                {"a", "b///", "a/b"},
+                {"a", "/b", "a/b"},
+                {"a", "/b/", "a/b"},
+                {"a", "/b///", "a/b"},
+                {"a", "//b", "a/b"},
+                {"a", "//b/", "a/b"},
+                {"a", "//b//", "a/b"},
+                {"a", "//b///", "a/b"},
+                {"a/", "b", "a/b"},
+                {"a/", "b/", "a/b"},
+                {"a/", "b//", "a/b"},
+                {"a/", "b///", "a/b"},
+                {"a/", "/b", "a/b"},
+                {"a/", "/b/", "a/b"},
+                {"a/", "/b///", "a/b"},
+                {"a/", "//b", "a/b"},
+                {"a/", "//b/", "a/b"},
+                {"a/", "//b//", "a/b"},
+                {"a/", "//b///", "a/b"},
+                {"a//", "b", "a/b"},
+                {"a//", "b/", "a/b"},
+                {"a//", "b//", "a/b"},
+                {"a//", "b///", "a/b"},
+                {"a//", "/b", "a/b"},
+                {"a//", "/b/", "a/b"},
+                {"a//", "/b///", "a/b"},
+                {"a//", "//b", "a/b"},
+                {"a//", "//b/", "a/b"},
+                {"a//", "//b//", "a/b"},
+                {"a//", "//b///", "a/b"},
+                {"/a//", "b", "/a/b"},
+                {"/a//", "b/", "/a/b"},
+                {"/a//", "b//", "/a/b"},
+                {"/a//", "b///", "/a/b"},
+                {"/a//", "/b", "/a/b"},
+                {"/a//", "/b/", "/a/b"},
+                {"/a//", "/b///", "/a/b"},
+                {"/a//", "//b", "/a/b"},
+                {"//a//", "//b/", "/a/b"},
+                {"///a//", "//b//", "/a/b"},
+                {"/////a//", "//b///", "/a/b"},
+                {"a", "a", "a/a"},
+                {"//a", "/a", "/a/a"},
+                {"/a", "/a", "/a/a"},
+                {"/a", "hello world", "/a/hello world"},
+                {"/a/✌", "你 好️", "/a/✌/你 好️"},
+        });
+    }
+
+    @Test
+    public void concatenated_path_is_as_expected() throws Exception {
+        String actual = Path.fromString(basePath).concat(additionalPath).toString();
+        assertEquals(expectedPath, actual);
+    }
+}
