647c10608a86f0360b5c2ae147376865b45885c6
Lae <laec@protonmail.ch>
2017-01-17 22:25:37 +1300

    Fix compilation.

--- ui-browser/src/androidTest/java/l/files/ui/browser/RenameTest.java
+++ ui-browser/src/androidTest/java/l/files/ui/browser/RenameTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 
 import l.files.fs.Path;
 import l.files.fs.Permission;
+import l.files.testing.fs.ExtendedPath;
 
 import static java.lang.Thread.sleep;
 import static java.util.concurrent.TimeUnit.SECONDS;
@@ -34,7 +35,7 @@ public class RenameTest extends BaseFilesActivityTest {
 
         try {
 
-            Path src = fs.createFile(dir.concat("file.txt"));
+            Path src = dir.concat("file.txt").createFile();
             Path dst = dir.concat("file.TXT");
 
             UiRename ui = rename(src);
@@ -48,14 +49,14 @@ public class RenameTest extends BaseFilesActivityTest {
                     .assertAllItemsDisplayedInOrder(dst);
 
         } finally {
-            fs.deleteRecursiveIfExists(dir);
+            ExtendedPath.wrap(dir).deleteRecursiveIfExists();
         }
     }
 
     @Test
     public void shows_error_when_failed_to_rename() throws Exception {
-        Path file = fs.createFile(dir().concat("a"));
-        fs.removePermissions(dir(), Permission.write());
+        Path file = dir().concat("a").createFile();
+        dir().removePermissions(Permission.write());
         rename(file)
                 .setFilename("abc")
                 .okExpectingFailure("Permission denied");
@@ -63,7 +64,7 @@ public class RenameTest extends BaseFilesActivityTest {
 
     @Test
     public void renames_file_to_specified_name() throws Throwable {
-        final Path from = fs.createFile(dir().concat("a"));
+        final Path from = dir().concat("a").createFile();
         final Path to = dir().concat("abc");
 
         rename(from).setFilename(to.name().toString()).ok();
@@ -71,36 +72,36 @@ public class RenameTest extends BaseFilesActivityTest {
         timeout(10, SECONDS, new Executable() {
             @Override
             public void execute() throws Exception {
-                assertFalse(fs.exists(from, NOFOLLOW));
-                assertTrue(fs.exists(to, NOFOLLOW));
+                assertFalse(from.exists(NOFOLLOW));
+                assertTrue(to.exists(NOFOLLOW));
             }
         });
     }
 
     @Test
     public void highlights_file_base_name_in_dialog() throws Exception {
-        Path file = fs.createFile(dir().concat("abc.txt"));
+        Path file = dir().concat("abc.txt").createFile();
         rename(file).assertSelection("abc");
     }
 
     @Test
     public void uses_filename_as_default_text() throws Exception {
-        Path file = fs.createFile(dir().concat("a"));
+        Path file = dir().concat("a").createFile();
         rename(file).assertFilename(file.name().toString());
     }
 
     @Test
     public void disables_ok_button_with_no_error_initially_because_we_use_source_filename_as_suggestion()
             throws Exception {
-        rename(fs.createDir(dir().concat("a")))
+        rename(dir().concat("a").createDir())
                 .assertOkButtonEnabled(false)
                 .assertHasNoError();
     }
 
     @Test
     public void cannot_rename_if_new_name_exists() throws Exception {
-        fs.createFile(dir().concat("abc"));
-        rename(fs.createFile(dir().concat("a")))
+        dir().concat("abc").createFile();
+        rename(dir().concat("a").createFile())
 
                 .setFilename("abc")
                 .assertOkButtonEnabled(false)
@@ -115,8 +116,8 @@ public class RenameTest extends BaseFilesActivityTest {
     public void rename_button_is_disable_if_there_are_more_than_one_file_checked()
             throws Exception {
 
-        Path f1 = fs.createDir(dir().concat("dir"));
-        Path f2 = fs.createFile(dir().concat("a"));
+        Path f1 = dir().concat("dir").createDir();
+        Path f2 = dir().concat("a").createFile();
 
         screen()
                 .longClick(f1)
