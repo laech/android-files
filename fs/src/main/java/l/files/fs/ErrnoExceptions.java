6b5e8486c9c091df0f11e8b3609734df728e9c76
Lae <laec@protonmail.ch>
2017-02-07 21:34:48 +1300

    Remove unused.

--- fs/src/main/java/l/files/fs/ErrnoExceptions.java
+++ fs/src/main/java/l/files/fs/ErrnoExceptions.java
@@ -8,11 +8,10 @@ import l.files.fs.exception.AccessDenied;
 import l.files.fs.exception.AlreadyExist;
 import l.files.fs.exception.DirectoryNotEmpty;
 import l.files.fs.exception.IsDirectory;
-import l.files.fs.exception.TooManySymbolicLinks;
 import l.files.fs.exception.NameTooLong;
 import l.files.fs.exception.NoSuchEntry;
 import l.files.fs.exception.NotDirectory;
-import l.files.fs.exception.PermissionDenied;
+import l.files.fs.exception.TooManySymbolicLinks;
 import linux.ErrnoException;
 
 import static linux.Errno.EACCES;
@@ -23,7 +22,6 @@ import static linux.Errno.ENAMETOOLONG;
 import static linux.Errno.ENOENT;
 import static linux.Errno.ENOTDIR;
 import static linux.Errno.ENOTEMPTY;
-import static linux.Errno.EPERM;
 
 final class ErrnoExceptions {
 
@@ -38,7 +36,6 @@ final class ErrnoExceptions {
         if (cause.errno == ENAMETOOLONG) return new NameTooLong(message, cause);
         if (cause.errno == ENOENT) return new NoSuchEntry(message, cause);
         if (cause.errno == ENOTDIR) return new NotDirectory(message, cause);
-        if (cause.errno == EPERM) return new PermissionDenied(message, cause);
         if (cause.errno == EISDIR) return new IsDirectory(message, cause);
         if (cause.errno == ENOTEMPTY) return new DirectoryNotEmpty(message, cause);
         return new IOException(message, cause);
