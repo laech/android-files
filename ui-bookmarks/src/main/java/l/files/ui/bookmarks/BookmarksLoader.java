6c478de98ff94971df04fe4ec73fc22edb47af70
Lae <laec@protonmail.ch>
2018-02-20 21:42:06 +1300

    Refactor.

--- ui-bookmarks/src/main/java/l/files/ui/bookmarks/BookmarksLoader.java
+++ ui-bookmarks/src/main/java/l/files/ui/bookmarks/BookmarksLoader.java
@@ -1,20 +1,18 @@
 package l.files.ui.bookmarks;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.content.AsyncTaskLoader;
 
 import java.text.Collator;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-
-import android.support.annotation.Nullable;
 
 import l.files.bookmarks.BookmarkManager;
 import l.files.fs.Path;
 import l.files.ui.base.text.CollationKey;
 
+import static java.util.Collections.sort;
+import static l.files.base.Lists.map;
 import static l.files.base.Objects.requireNonNull;
 import static l.files.bookmarks.BookmarkManager.BookmarkChangedListener;
 
@@ -22,7 +20,7 @@ final class BookmarksLoader extends AsyncTaskLoader<List<Path>> {
 
     private final Path home;
     private final BookmarkManager manager;
-    private final BookmarkChangedListener listener;
+    private final BookmarkChangedListener listener = manager -> forceLoad();
 
     @Nullable
     private List<Path> bookmarks;
@@ -31,33 +29,23 @@ final class BookmarksLoader extends AsyncTaskLoader<List<Path>> {
         super(context);
         this.home = requireNonNull(home);
         this.manager = requireNonNull(manager);
-        this.listener = new BookmarkListener();
     }
 
     @Override
     public List<Path> loadInBackground() {
         Collator collator = Collator.getInstance();
         List<Entry> collation = collateBookmarks(collator);
-        List<Path> bookmarks = new ArrayList<>(collation.size());
-        for (Entry pair : collation) {
-            bookmarks.add(pair.bookmark);
-        }
-        return bookmarks;
+        return map(collation, pair -> pair.bookmark);
     }
 
-    @NonNull
     private List<Entry> collateBookmarks(Collator collator) {
-        List<Entry> collation = new ArrayList<>();
-        for (Path bookmark : manager.getBookmarks()) {
-            collation.add(new Entry(bookmark, CollationKey.create(collator, bookmark.getName().or(""))));
-        }
-        Collections.sort(collation, (a, b) -> {
-            if (a.bookmark.equals(home)) {
-                return -1;
-            }
-            if (b.bookmark.equals(home)) {
-                return 1;
-            }
+        List<Entry> collation = map(manager.getBookmarks(), bookmark -> {
+            String name = bookmark.getName().or("");
+            return new Entry(bookmark, CollationKey.create(collator, name));
+        });
+        sort(collation, (a, b) -> {
+            if (a.bookmark.equals(home)) return -1;
+            if (b.bookmark.equals(home)) return +1;
             return a.collationKey.compareTo(b.collationKey);
         });
         return collation;
@@ -84,13 +72,6 @@ final class BookmarksLoader extends AsyncTaskLoader<List<Path>> {
     public void deliverResult(List<Path> data) {
         super.deliverResult(data);
         this.bookmarks = data;
-    }
-
-    private final class BookmarkListener implements BookmarkChangedListener {
-        @Override
-        public void onBookmarkChanged(BookmarkManager manager) {
-            forceLoad();
-        }
     }
 
     private static final class Entry {
