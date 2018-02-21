7c85f0ca5c15a9b720b7872d518ebba82dfcc721
Lae <laec@protonmail.ch>
2018-02-21 21:15:01 +1300

    Refactor.

--- ui-browser/src/main/java/l/files/ui/browser/OpenFile.java
+++ ui-browser/src/main/java/l/files/ui/browser/OpenFile.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 
 import l.files.fs.Path;
 import l.files.fs.Stat;
@@ -21,18 +22,20 @@ import static l.files.ui.base.fs.IOExceptions.message;
 
 final class OpenFile extends AsyncTask<Void, Void, Object> {
 
-    private final Context context;
+    private final WeakReference<Context> contextRef;
     private final Path file;
     private final Stat stat;
 
     OpenFile(Context context, Path file, Stat stat) {
-        this.context = requireNonNull(context);
+        this.contextRef = new WeakReference<>(context);
         this.file = requireNonNull(file);
         this.stat = requireNonNull(stat);
     }
 
     @Override
     protected Object doInBackground(Void... params) {
+        Context context = contextRef.get();
+        if (context == null) return null;
         try {
             return MediaTypes.detect(context, file, stat);
         } catch (IOException e) {
@@ -42,24 +45,26 @@ final class OpenFile extends AsyncTask<Void, Void, Object> {
 
     @Override
     protected void onPostExecute(Object result) {
+        Context context = contextRef.get();
+        if (context == null) return;
+        if (result == null) return;
         if (result instanceof IOException) {
-            showException((IOException) result);
+            showException((IOException) result, context);
             return;
         }
-
-        if (!showFile((String) result) &&
-                !showFile(MediaTypes.generalize((String) result))) {
-            showFile(MEDIA_TYPE_ANY);
+        if (!showFile((String) result, context) &&
+                !showFile(MediaTypes.generalize((String) result), context)) {
+            showFile(MEDIA_TYPE_ANY, context);
         }
     }
 
-    private void showException(IOException exception) {
+    private void showException(IOException exception, Context context) {
         String msg = message(exception);
         makeText(context, msg, LENGTH_SHORT).show();
     }
 
-    private boolean showFile(String media) {
-        debug(media);
+    private boolean showFile(String media, Context context) {
+        debug(media, context);
 
         try {
             Intent intent = new Intent(ACTION_VIEW);
@@ -71,7 +76,7 @@ final class OpenFile extends AsyncTask<Void, Void, Object> {
         }
     }
 
-    private void debug(String media) {
+    private void debug(String media, Context context) {
         if (isDebugBuild(context)) {
             makeText(context, "[DEBUG] " + media, LENGTH_SHORT).show();
         }
