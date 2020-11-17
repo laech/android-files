0d490dea2c2a8650f417b4e847f4b596a5f0fbdc
Lae Chen <laec@protonmail.ch>
2020-11-17 20:52:44 +1300

    Remove some custom implementations.

--- ui-base/src/main/java/l/files/ui/base/fs/FileIcons.java
+++ ui-base/src/main/java/l/files/ui/base/fs/FileIcons.java
@@ -2,35 +2,16 @@ package l.files.ui.base.fs;
 
 import androidx.annotation.DrawableRes;
 
-import l.files.fs.Path;
+import java.nio.file.Path;
 
-import static l.files.ui.base.R.drawable.ic_file_download_black_24dp;
-import static l.files.ui.base.R.drawable.ic_folder_black_24dp;
-import static l.files.ui.base.R.drawable.ic_home_black_24dp;
-import static l.files.ui.base.R.drawable.ic_insert_drive_file_black_24dp;
-import static l.files.ui.base.R.drawable.ic_library_music_black_24dp;
-import static l.files.ui.base.R.drawable.ic_phone_android_black_24dp;
-import static l.files.ui.base.R.drawable.ic_photo_library_black_24dp;
-import static l.files.ui.base.R.drawable.ic_sd_storage_black_24dp;
-import static l.files.ui.base.R.drawable.ic_video_library_black_24dp;
-import static l.files.ui.base.fs.UserDirs.DIR_DCIM;
-import static l.files.ui.base.fs.UserDirs.DIR_DOWNLOADS;
-import static l.files.ui.base.fs.UserDirs.DIR_HOME;
-import static l.files.ui.base.fs.UserDirs.DIR_MOVIES;
-import static l.files.ui.base.fs.UserDirs.DIR_MUSIC;
-import static l.files.ui.base.fs.UserDirs.DIR_PICTURES;
-import static l.files.ui.base.fs.UserDirs.DIR_ROOT;
-import static l.files.ui.base.fs.UserDirs.DIR_SDCARD2;
+import static l.files.ui.base.R.drawable.*;
+import static l.files.ui.base.fs.UserDirs.*;
 
 public final class FileIcons {
 
     private FileIcons() {
     }
 
-    @DrawableRes
-    public static int getDirectory(java.nio.file.Path p) {
-        return getDirectory(Path.of(p));
-    }
 
     @DrawableRes
     public static int getDirectory(Path p) {
--- ui-base/src/main/java/l/files/ui/base/fs/FileLabels.java
+++ ui-base/src/main/java/l/files/ui/base/fs/FileLabels.java
@@ -2,10 +2,9 @@ package l.files.ui.base.fs;
 
 import android.content.res.Resources;
 import android.os.Build;
-
-import l.files.base.Optional;
-import l.files.fs.Path;
 import l.files.ui.base.R;
+
+import java.nio.file.Path;
 
 import static l.files.ui.base.fs.UserDirs.DIR_HOME;
 import static l.files.ui.base.fs.UserDirs.DIR_ROOT;
@@ -15,15 +14,11 @@ public final class FileLabels {
     private FileLabels() {
     }
 
-    public static String get(Resources res, java.nio.file.Path path) {
-        return get(res, Path.of(path));
-    }
-
     public static String get(Resources res, Path file) {
         if (DIR_HOME.equals(file)) return res.getString(R.string.home);
         if (DIR_ROOT.equals(file)) return Build.MODEL;
-        Optional<String> name = file.toAbsolutePath().getName();
-        return String.valueOf(name.orObject(file));
+        Path fileName = file.toAbsolutePath().getFileName();
+        return fileName != null ? fileName.toString() : file.toString();
     }
 
 }
